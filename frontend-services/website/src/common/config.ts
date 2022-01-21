7b75fac6090071f55ec421ba18eddca488588aa0
Johannes Nau <johannes.nau@outlook.com>
2022-01-21 10:31:23 +0100

    clean up and relocated development function

--- frontend-services/website/src/common/config.ts
+++
@@ -1,7 +0,0 @@
-export interface GoldiServiceConfig {
-    PORT: number,
-    NODE_ENV: "production" | "development" | "install" | string
-    API_URL: string,
-    SECURITY_ISSUER: string | undefined,
-    SECURITY_AUDIENCE: string | undefined
-}
--- frontend-services/website/src/config.ts
+++ frontend-services/website/src/config.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const config = {
-    PORT: process.env.GOLDI_API_PORT ? parseInt(process.env.GOLDI_API_PORT) : 3000,
+    PORT: process.env.GOLDI_FRONTEND_PORT ? parseInt(process.env.GOLDI_FRONTEND_PORT) : 9000,
     NODE_ENV: process.env.NODE_ENV || "development",
     API_URL: process.env.GOLDI_API_URL ?? "http://localhost:3000",
     SECURITY_ISSUER: process.env.GOLDI_SECURITY_ISSUER!,
