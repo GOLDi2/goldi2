483ffa7db72c5ee38534d6a7042758cf397aa9bb
Johannes Nau <johannes.nau@outlook.com>
2022-03-29 12:50:56 +0200

    styling and small bug fixes

--- hardware/common/config-interface/src/index.ts
+++ hardware/common/config-interface/src/index.ts
@@ -5,12 +5,13 @@ import { spawnSync } from 'child_process';
 import { config } from "./config";
 import path from 'path';
 import { renderPageInit } from "./utils";
+import { AddressInfo } from "net";
 
 const app = express();
 
-const firmware_upload = multer({dest: '/tmp/'});
+const firmware_upload = multer({ dest: '/tmp/' });
 
-const content_path = config.NODE_ENV === 'development' ? 'src/content' : 'dist/content';
+const content_path = path.join(__dirname, '..', config.NODE_ENV === 'development' ? 'src' : 'dist', 'content');
 const nunjucks_configuration = {
     autoescape: true,
     noCache: config.NODE_ENV === 'development'
@@ -23,9 +24,10 @@ const renderPage = renderPageInit(content_path, config.DEFAULT_LANGUAGE);
 
 let router = express.Router();
 
-router.post('/upload_firmware', firmware_upload.single('file'), async ( req, res, next ) => {
-    let {stderr, stdout} = spawnSync(`rauc install ${(req as any).file.path}`, {shell: true});
-    await renderPage(req.path, (req as any).language, res, {stderr, stdout});
+router.post('/upload_firmware', firmware_upload.single('file'), async (req, res, next) => {
+    console.log(req)
+    let { stderr, stdout } = spawnSync(`rauc install ${(req as any).file.path}`, { shell: true });
+    res.send(stdout.toString()+stderr.toString());
 });
 
 router.use('/', async (req, res) => { await renderPage(req.path, (req as any).language, res); });
@@ -46,11 +48,18 @@ if (config.NODE_ENV === 'development') {
 // HTML paths
 for (const language of languages) {
     app.get('/' + language + '/', async (req, res) => { await renderPage('index', language, res); });
-    app.use('/' + language + '/', async (req, res, next) => { (req as any).language=language; next(); }, router);
+    app.use('/' + language + '/', async (req, res, next) => { (req as any).language = language; next(); }, router);
 }
 app.get('/', function (req, res) {
     const selected_language = req.acceptsLanguages(languages) || config.DEFAULT_LANGUAGE;
     res.redirect(307, '/' + selected_language + req.originalUrl);
 });
 
-app.listen( config.PORT );
+if (config.NODE_ENV === 'development') {
+    // When developing, we start a browserSync server after listen
+    const { start_browserSync } = require("./debug_utils");
+    const server = app.listen(() => start_browserSync((server.address() as AddressInfo).port));
+} else {
+    // Just listen on the configured port
+    app.listen(config.PORT);
+}
