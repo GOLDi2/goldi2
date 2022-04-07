9e2fb33d6d43ab205fd0954b8605c93d6e7ca125
Pierre Helbing <pierre.helbing@tu-ilmenau.de>
2022-04-07 09:32:40 +0200

    add debug messages

--- hardware/common/svf-player/src/execute.c
+++ hardware/common/svf-player/src/execute.c
@@ -327,6 +327,7 @@ static int _shift(SVF_Shift_Data* instr, char* data, int exit)
         }
         for (int i = 0; i < instr->length; i++)
         {
+            printf("data: %d, pos: %02x, shift: %d \n");
             data[j] |= clk((exit && (i == instr->length-1) && (j == 0)), (instr->tdi[j] & pos) >> shift) << shift;
             shift++;
             pos *= 2;
@@ -349,6 +350,7 @@ static int _shift(SVF_Shift_Data* instr, char* data, int exit)
     if (instr->tdo)
     {
         print_hexstring(data, (instr->length/8) + ((instr->length % 8) > 0));
+        printf("\n");
         if (instr->mask)
         {
             for (unsigned int i = 0; i < (instr->length/8) + ((instr->length % 8) > 0); i++)
