e56e8a304554d5e76353c24edbd84dc1de17c938
Pierre Helbing <pierre.helbing@tu-ilmenau.de>
2022-05-06 16:58:22 +0200

    higher timeout for mc gpio test

--- hardware/common/board-test/src/testcases/mc_gpio_test/index.ts
+++ hardware/common/board-test/src/testcases/mc_gpio_test/index.ts
@@ -10,14 +10,14 @@ const testCaseData: TestCaseData = {
     description: "Tests if the GPIOs of the Microcontroller are working properly.",
     test: (tc: TestCase) => {
         return new Promise<void>(resolve => {
-            const proc_0 = spawnSync(`svf-player ${generatedTestsDir}/test_fpga_mc_0.svf -ni`, {timeout: 10000, shell: true})
+            const proc_0 = spawnSync(`svf-player ${generatedTestsDir}/test_fpga_mc_0.svf -ni`, {timeout: 120000, shell: true})
             if (proc_0.status != 0) {
                 tc.outcome = "Fail"
                 const str = fs.readFileSync("/tmp/svf_output.json", {encoding: "utf-8"})
                 const faults = JSON.parse(str)["faults"]
                 check_values(faults, bsdl_fpga, "faults_mc_gpio_0")
             }
-            const proc_1 = spawnSync(`svf-player ${generatedTestsDir}/test_fpga_mc_1.svf -ni`, {timeout: 10000, shell: true})
+            const proc_1 = spawnSync(`svf-player ${generatedTestsDir}/test_fpga_mc_1.svf -ni`, {timeout: 120000, shell: true})
             if (proc_1.status != 0) {
                 tc.outcome = "Fail"
                 const str = fs.readFileSync("/tmp/svf_output.json", {encoding: "utf-8"})
