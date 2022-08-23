2a48b2f9be81858b489e59e23d65278583595505
Johannes Nau <johannes.nau@outlook.com>
2022-08-23 11:49:59 +0200

    implements login

--- frontend-services/website/src/index.ts
+++ frontend-services/website/src/index.ts
@@ -1,9 +1,22 @@
 import { config } from "./config"
-import express, { Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import nunjucks from 'nunjucks';
 import path from 'path';
 import { AddressInfo } from "net";
 import { renderPageInit } from "./utils";
+import { APIClient } from "@cross-lab-project/api-client"
+import { User } from "@cross-lab-project/api-client/dist/generated/auth/types";
+import expressWinston from 'express-winston';
+import winston from 'winston';
+import cookieParser from 'cookie-parser'
+import asyncHandler from 'express-async-handler';
+
+declare module 'express-serve-static-core' {
+    interface Request {
+        apiClient: APIClient
+        user?: User
+    }
+}
 
 
 const content_path = config.NODE_ENV === 'development' ? 'src/content' : 'dist/content';
@@ -15,12 +28,22 @@ const languages = ['en', 'de'];
 
 const renderPage = renderPageInit(content_path, config.DEFAULT_LANGUAGE);
 
-
-
 const app = express();
+
+const logger = winston.createLogger({
+    transports: [
+        new winston.transports.Console()
+    ],
+    format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+    ),
+})
 
 app.set('etag', config.NODE_ENV !== 'development');
 app.use((_req, res, next) => { res.removeHeader('X-Powered-By'); next(); });
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 nunjucks.configure(content_path + '/templates', { ...nunjucks_configuration, express: app });
 
@@ -33,16 +56,73 @@ if (config.NODE_ENV === 'development') {
     // For production, we use precompiled css files
     app.use("/css", express.static(path.join(content_path, 'css')));
 }
-for (const language of languages) {
-    app.get('/' + language + '/', async (req, res) => { await renderPage('index', language, res); });
-    app.use('/' + language + '/', async (req, res) => { await renderPage(req.path, language, res); });
+
+app.use(expressWinston.logger({
+    winstonInstance: logger,
+    meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+    msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+    expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+    colorize: true, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+    ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
+}));
+
+async function handle_login(req: Request, res: Response, next: NextFunction) {
+    const response = await req.apiClient.postLogin({ username: req.body.username, password: req.body.password, method: "tui" });
+    if (response.status == 201) {
+        res.cookie('token', req.apiClient.accessToken, { secure: true, httpOnly: true, sameSite: 'strict' })
+        req.user = {
+            username: "Username TO BE CHANGED"
+        }
+
+        if (req.query.redirect ){
+            res.redirect(303, req.query.redirect as string);
+        }else{
+            next();
+        }
+    } else {
+        res.clearCookie('token')
+        next()
+    }
 }
-app.get('/', function (req, res) {
+
+async function handle_logout(req: Request, res: Response, next: NextFunction) {
+    res.clearCookie('token')
+    if (req.query.redirect ){
+        res.redirect(303, req.query.redirect as string);
+    }else{
+        next();
+    }
+}
+
+app.use('/', asyncHandler(async (req: Request, res, next) => {
+    req.apiClient = new APIClient(config.API_URL)
+    req.user = undefined
+    if (req.cookies.token) {
+        req.apiClient.accessToken = req.cookies.token
+        /*const identityResponse = await req.apiClient.getIdentity()
+        if (identityResponse.status == 200) {
+            req.user = identityResponse.body
+        }*/
+        req.user = {
+            username: "Username TO BE CHANGED"
+        }
+    }
+    next()
+}))
+
+for (const language of languages) {
+    app.post('/' + language + '/login.html', asyncHandler(handle_login));
+    app.get('/' + language + '/logout.html', asyncHandler(handle_logout));
+    app.get('/' + language + '/', asyncHandler(async (req, res) => { await renderPage('index', language, res, req.user); }));
+    app.use('/' + language + '/', asyncHandler(async (req, res) => { await renderPage(req.path, language, res, req.user); }));
+}
+
+app.use('/', function (req, res) {
     const selected_language = req.acceptsLanguages(languages) || config.DEFAULT_LANGUAGE;
     res.redirect(307, '/' + selected_language + req.originalUrl);
 });
 
-
+app.use(expressWinston.errorLogger({ winstonInstance: logger }));
 
 if (config.NODE_ENV === 'development') {
     // When developing, we start a browserSync server after listen
--- frontend-services/website/src/utils.ts
+++ frontend-services/website/src/utils.ts
@@ -2,18 +2,19 @@ import path from "path";
 import fs from "fs";
 
 import { Response } from "express";
+import { User } from "@cross-lab-project/api-client/dist/generated/auth/types";
 
 export function template_resolution(base_path: string, sub_path: string, default_language: string) {
-    return async (filename:string, language?: string)=>{
-        if(!language) language=default_language;
-        filename=await find_language_suffixed_files(path.join(base_path, sub_path, filename), language, default_language, '.html');
+    return async (filename: string, language?: string) => {
+        if (!language) language = default_language;
+        filename = await find_language_suffixed_files(path.join(base_path, sub_path, filename), language, default_language, '.html');
         return path.relative(base_path, filename);
     }
 }
 
 export async function find_language_suffixed_files(filename: string, language: string, default_language: string = 'en', overwrite_extension?: string) {
     let ext_name = path.extname(filename);
-    let basename = ext_name.length>0?filename.slice(0, -ext_name.length):filename;
+    let basename = ext_name.length > 0 ? filename.slice(0, -ext_name.length) : filename;
     if (basename.endsWith('.')) basename = basename.slice(0, -1);
     if (overwrite_extension) ext_name = overwrite_extension;
     if (!ext_name.startsWith('.')) ext_name = '.' + ext_name;
@@ -24,20 +25,23 @@ export async function find_language_suffixed_files(filename: string, language: s
             return filename;
         }
     }
-    
+
     throw new Error('Could not find file ' + filename);
 }
 
 
-export function renderPageInit(content_path: string, default_language: string){
-    const page_resolution = template_resolution(content_path+'/templates', 'pages', default_language);
-    return async function renderPage(page:string, language: string, res:Response) {
+export function renderPageInit(content_path: string, default_language: string) {
+    const page_resolution = template_resolution(content_path + '/templates', 'pages', default_language);
+
+    return async function renderPage(page: string, language: string, res: Response, user?: User) {
+        const isLoggedIn = user !== undefined
+        const _page=page.slice(1)
         try {
             const page_template = await page_resolution(page, language);
-            res.render(page_template, { language });
+            res.render(page_template, { language, page:_page, isLoggedIn, user });
         } catch (e) {
             const page_template = await page_resolution("404", language);
-            res.render(page_template, { language });
+            res.render(page_template, { language, page:_page, isLoggedIn, user });
         }
     }
 }
