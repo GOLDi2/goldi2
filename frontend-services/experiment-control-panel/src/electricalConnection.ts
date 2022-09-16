8442cc7061528f083136c710d64284b630e54e01
Johannes Nau <johannes.nau@outlook.com>
2022-09-16 15:16:49 +0200

    prototypical version

--- frontend-services/experiment-control-panel/src/electricalConnection.ts
+++ frontend-services/experiment-control-panel/src/electricalConnection.ts
@@ -1,29 +1,49 @@
-import { DeviceHandler } from '@cross-lab-project/soa-client';
-import { ElectricalConnectionService, GPIO } from '@cross-lab-project/soa-service-electrical';
-import { GPIOInterface, GPIOState } from '@cross-lab-project/soa-service-electrical/dist/gpio';
-import { LitElement, html } from 'lit';
-import { customElement, state } from 'lit/decorators.js';
-import produce from "immer"
+import { DeviceHandler } from "@cross-lab-project/soa-client";
+import {
+  ElectricalConnectionService,
+  GPIO,
+} from "@cross-lab-project/soa-service-electrical";
+import {
+  GPIOInterface,
+  GPIOState,
+} from "@cross-lab-project/soa-service-electrical/dist/gpio";
+import { LitElement, html, adoptStyles, unsafeCSS } from "lit";
+import { customElement, state } from "lit/decorators.js";
+import produce from "immer";
 
+import style from "./styles.css";
+const stylesheet = unsafeCSS(style);
 
-@customElement('soa-electrical-connection')
+@customElement("soa-electrical-connection")
 export class ElectricalConnection extends LitElement {
+  async connectedCallback() {
+    super.connectedCallback();
+    adoptStyles(this.shadowRoot, [stylesheet]);
+  }
+
+  name: string;
+  constructor(name: string) {
+    super();
+    this.name = name;
+  }
+
   @state()
   protected _signals: { [key: string]: GPIOState } = {};
   @state()
   protected _signals_driver: { [key: string]: GPIOState } = {};
 
-  protected _interfaces: { [key: string]: GPIOInterface }={};
+  protected _interfaces: { [key: string]: GPIOInterface } = {};
 
   register(deviceHandler: DeviceHandler) {
-    const sensor_service = new ElectricalConnectionService("sensors", []);
-    const sensor_interface = new GPIO.ConstructableGPIOInterface([])
+    const sensor_service = new ElectricalConnectionService(this.name, []);
+    const sensor_interface = new GPIO.ConstructableGPIOInterface([]);
     sensor_service.addInterface(sensor_interface);
 
     sensor_service.on("newInterface", (event) => {
       if (event.connectionInterface.interfaceType == "gpio") {
         const gpioInterface = event.connectionInterface as GPIOInterface;
-        this._interfaces[gpioInterface.configuration.signals.gpio]=gpioInterface;
+        this._interfaces[gpioInterface.configuration.signals.gpio] =
+          gpioInterface;
         this._signals = produce(this._signals, (draft) => {
           draft[gpioInterface.configuration.signals.gpio] = GPIOState.Unknown;
         });
@@ -31,16 +51,16 @@ export class ElectricalConnection extends LitElement {
           this._signals = produce(this._signals, (draft) => {
             draft[gpioInterface.configuration.signals.gpio] = signalEvent.state;
           });
-        })
+        });
       }
-    })
+    });
 
     deviceHandler.addService(sensor_service);
   }
 
   private _rotateDriver(name: string) {
     this._signals_driver = produce(this._signals_driver, (draft) => {
-      const old_state=draft[name] || GPIOState.HighZ;
+      const old_state = draft[name] || GPIOState.HighZ;
       let new_state = GPIOState.Unknown;
       switch (old_state) {
         case GPIOState.HighZ:
@@ -59,12 +79,64 @@ export class ElectricalConnection extends LitElement {
     });
   }
 
-  private _renderSignal(name: string, state: GPIOState, driver: GPIOState = GPIOState.HighZ) {
+  private _driveHigh(name: string) {
+    this._signals_driver = produce(this._signals_driver, (draft) => {
+      let new_state = GPIOState.StrongHigh;
+      this._interfaces[name].changeDriver(new_state);
+      draft[name] = new_state;
+    });
+  }
+
+  private _driveLow(name: string) {
+    this._signals_driver = produce(this._signals_driver, (draft) => {
+      let new_state = GPIOState.StrongLow;
+      this._interfaces[name].changeDriver(new_state);
+      draft[name] = new_state;
+    });
+  }
+
+  private _driveHighZ(name: string) {
+    this._signals_driver = produce(this._signals_driver, (draft) => {
+      let new_state = GPIOState.HighZ;
+      this._interfaces[name].changeDriver(new_state);
+      draft[name] = new_state;
+    });
+  }
+
+  private _renderSignal(
+    name: string,
+    state: GPIOState,
+    driver: GPIOState = GPIOState.HighZ
+  ) {
+    let color = "bg-primary-50";
+    if (state == GPIOState.StrongHigh || state == GPIOState.WeakHigh) {
+      color = "bg-secondary";
+    } else if (state == GPIOState.StrongLow || state == GPIOState.WeakLow) {
+      color = "bg-primary-100";
+    }
     return html`
-      <div>
-        <span>Signal Name: ${name}</span>
-        <span>Current State: ${GPIOState[state]}</span>
-        <span @click="${()=>this._rotateDriver(name)}">Driver: ${GPIOState[driver]}</span>
+      <div class="flex w-[32rem] p-1">
+        <div class="grow"></div>
+        <div class="p-1">${name}</div>
+        <div class="${color} h-7 text-lg w-28 text-center rounded-l-lg m-0">
+          ${GPIOState[state]}
+        </div>
+        <button
+          class="bg-primary h-7 text-sm w-28 text-center m-0 border-l-2 border-r-2 active:bg-secondary"
+          @mousedown="${() => this._driveHigh(name)}"
+          @mouseup="${() => this._driveHighZ(name)}"
+          @mouseleave="${() => this._driveHighZ(name)}"
+        >
+          Drive High
+        </button>
+        <button
+          class="bg-primary h-7 text-sm w-28 text-center rounded-r-lg active:bg-secondary"
+          @mousedown="${() => this._driveLow(name)}"
+          @mouseup="${() => this._driveHighZ(name)}"
+          @mouseleave="${() => this._driveHighZ(name)}"
+        >
+          Drive Low
+        </button>
       </div>
     `;
   }
@@ -72,10 +144,18 @@ export class ElectricalConnection extends LitElement {
   render() {
     return html`
       <div>
-        <h1>Electrical Connection</h1>
-        ${Object.keys(this._signals).map((key) =>
-          this._renderSignal(key, this._signals[key], this._signals_driver[key])
-        )}
+        <h1 class="text-2xl text-center">
+          Electrical Connection (${this.name})
+        </h1>
+        <div class="flex flex-col w-full items-center">
+          ${Object.keys(this._signals).map((key) =>
+            this._renderSignal(
+              key,
+              this._signals[key],
+              this._signals_driver[key]
+            )
+          )}
+        </div>
       </div>
     `;
   }
--- frontend-services/experiment-control-panel/src/webcam.ts
+++ frontend-services/experiment-control-panel/src/webcam.ts
@@ -1,15 +1,22 @@
 import { DeviceHandler } from '@cross-lab-project/soa-client';
 import { WebcamService__Consumer } from '@cross-lab-project/soa-service-webcam';
-import { LitElement, html } from 'lit';
+import { LitElement, html, unsafeCSS, adoptStyles } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
 import produce from "immer"
 
-
+import style from "./styles.css";
+const stylesheet = unsafeCSS(style);
 @customElement('soa-electrical-webcam')
 export class Webcam extends LitElement {
+  async connectedCallback() {
+    super.connectedCallback();
+    adoptStyles(this.shadowRoot, [stylesheet]);
+  }
+
   register(deviceHandler: DeviceHandler) {
     const webcamService = new WebcamService__Consumer("webcam");
     webcamService.on("track", (event) => {
+        this.dispatchEvent(new CustomEvent("webcam", { detail: event.track, bubbles: true, composed: true }));
         const webcamDiv = this.shadowRoot.getElementById("webcam");
         const video = document.createElement('video');
         const mediaStream=new MediaStream([event.track])
@@ -25,9 +32,9 @@ export class Webcam extends LitElement {
 
   render() {
     return html`
-      <div>
-        <h1>Webcam</h1>
-        <div id="webcam"></div>
+      <div class="p-2">
+        <h1 class="text-2xl w-full text-center">Webcam</h1>
+        <div class="aspect-[4/3] bg-primary-50 w-full" id="webcam"></div>
       </div>
     `;
   }
