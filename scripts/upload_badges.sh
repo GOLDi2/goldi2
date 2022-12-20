64a93ec193750c31d165708981177b5554d191d1
Johannes Nau <johannes.nau@outlook.com>
2022-12-20 14:16:09 +0100

    fix badge generation

--- scripts/upload_badges.sh
+++ scripts/upload_badges.sh
@@ -14,8 +14,7 @@ badges=$(echo "$badges" | sed -e 's/_success.svg$//' -e 's/_failed.svg$//' -e 's
 mkdir -p badges
 for badge in $badges; do
     name=$(basename $badge)
-    echo "${badge}_${JOB_STATUS}.svg"
     cp "${badge}_${JOB_STATUS}.svg" "badges/${name}.svg" || cp "${badge}.svg" "badges/${name}.svg"
 done
 
-#rsync -e "ssh -o StrictHostKeyChecking=no" --rsync-path 'sudo rsync' -avP --info=progress2 --chmod=755 -L badges/* admin@x56.theoinf.tu-ilmenau.de:/data/www/x56/badges/
+rsync -e "ssh -o StrictHostKeyChecking=no" --rsync-path 'sudo rsync' -avP --info=progress2 --chmod=755 -L badges/* admin@x56.theoinf.tu-ilmenau.de:/data/www/x56/badges/
