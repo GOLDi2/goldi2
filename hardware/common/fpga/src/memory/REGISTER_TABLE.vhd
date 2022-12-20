8f85c45f3394f3769eb800ce4c27e24b9f2b4f4f
JP_CC <jose-pablo.chew-camey@tu-ilmenau.de>
2022-12-20 19:12:07 +0100

    V0.01.02 - refactor(MEMORY): Simplification of register table
    Simplification to the REGISTER_TABLE to eliminate superfluous
    design elements and to improve coding style.

--- hardware/common/fpga/src/memory/REGISTER_TABLE.vhd
+++ hardware/common/fpga/src/memory/REGISTER_TABLE.vhd
@@ -9,11 +9,21 @@
 -- Target Devices:	LCMXO2-7000HC-4TG144C
 -- Tool versions:	Lattice Diamond 3.12, Modelsim Lattice Edition
 --
--- Dependencies:	-> GOLDI_DATA_TYPES.vhd
+-- Dependencies:	-> GOLDI_COMM_STANDARD.vhd
 --
 -- Revisions:
--- Revision V1.00.00 - File Created
+-- Revision V0.01.00 - File Created
 -- Additional Comments: First commit
+--
+-- Revision V0.01.02 - Changes to the code stye, performance and documentation
+-- Additional Comments: Refactor of code to reduce code indents and complexity.
+--                      Addition of module header and comments.
+--                      Elimination of some module generics to introduce
+--						the GOLDI_COMM_STANDARD package which sets a
+--						unified standard for all communication modules
+--
+-- Revision V1.00.00 - Default module version for release 1.00.00
+-- Additional Comments: -
 -------------------------------------------------------------------------------
 --! Use standard library
 library IEEE;
@@ -22,22 +32,23 @@ use IEEE.numeric_std.all;
 --! Use assert library for synthesis
 use std.standard.all;
 --! Use custom library
-use work.GOLDI_DATA_TYPES.all;
+use work.GOLDI_COMM_STANDARD.all;
 
 
 
-
---! @brief
+--! @brief Configurable table of registers
 --! @details
+--! Table of registers with configurable base address, length and 
+--! initialization values. BUS interface reads the internal port input
+--! data and writes directly to the internal outut data registers while
+--! also flaging the internal system of the changes through the stb signals.
 --!
 --! **Latency: 1**
 entity REGISTER_TABLE is
 	generic(
 		BASE_ADDRESS		:	natural := 1;
-		REGISTER_NUMBER		:	natural := 3;
-		BUS_ADDRESS_WIDTH	:	natural := 8;
-		REG_CONFIGURATION	:	reg_type_array := (BI, W, R);
-		REG_DEFAULT_VALUES	:	word_8_bit_array := (x"00",x"00",x"00")
+		NUMBER_REGISTERS	:	natural := 3;
+		REG_DEFAULT_VALUES	:	data_word_vector := (x"00",x"00",x"00")
 	);
 	port(
 		--General
@@ -47,117 +58,98 @@ entity REGISTER_TABLE is
 		sys_bus_i		: in	bus_in;
 		sys_bus_o		: out	bus_out;
 		--Internal port
-		reg_data_in		: in	word_8_bit_array(REGISTER_NUMBER-1 downto 0);
-		reg_data_out	: out	word_8_bit_array(REGISTER_NUMBER-1 downto 0);
-		reg_data_stb	: out	std_logic_vector(REGISTER_NUMBER-1 downto 0)
+		reg_data_in		: in	data_word_vector(NUMBER_REGISTERS-1 downto 0);
+		reg_data_out	: out   data_word_vector(NUMBER_REGISTERS-1 downto 0);
+		reg_data_stb	: out	std_logic_vector(NUMBER_REGISTERS-1 downto 0)
 	);
 end entity REGISTER_TABLE;
 
 
 
-
 --! General architecture
 architecture RTL of REGISTER_TABLE is
-	
-	--Intermediate signals
-	--Constants
-	constant min_address	:	unsigned(BUS_ADDRESS_WIDTH-1 downto 0) 
-							:= to_unsigned(BASE_ADDRESS,BUS_ADDRESS_WIDTH);
+    --Intermediate signals
+    --Constants
+    constant min_address    :   unsigned(BUS_ADDRESS_WIDTH-1 downto 0) 
+							    := to_unsigned(BASE_ADDRESS,BUS_ADDRESS_WIDTH);
 	constant max_address	:	unsigned(BUS_ADDRESS_WIDTH-1 downto 0)
-							:= to_unsigned(BUS_ADDRESS_WIDTH+REGISTER_NUMBER,BUS_ADDRESS_WIDTH);
-	--Buffer
-	signal reg_buff			:	word_8_bit_array(REGISTER_NUMBER-1 downto 0);
-	signal reg_stb_buff		:	std_logic_vector(REGISTER_NUMBER-1 downto 0);
-	
-	
-begin
-	--*****************************************************************************************************************
-	--Synthesis checks
-	assert(BASE_ADDRESS > 0)
-		report "BASE_ADDRESS out of bounds - Expecting BASE_ADDRESS > 0" severity failure;
-	assert(REGISTER_NUMBER >= 1)
-		report "REGISTER_NUMBER out of bounds - Expecting REGISTER_NUMBER > 1" severity failure;
-	assert(REGISTER_NUMBER < 2**BUS_ADDRESS_WIDTH)
-		report "sys_bus_adr_i unable to access all registers - Expecting REGISTER_NUMBER < 2**BUS_ADDRESS_WIDTH"
-		severity failure;
-	assert(REG_CONFIGURATION'length = REGISTER_NUMBER)
-		report("REG_CONFIGURATION expects [" & integer'image(REGISTER_NUMBER) & "] elements")
-		severity failure;
-	assert(REG_DEFAULT_VALUES'length = REGISTER_NUMBER)
-		report("REG_DEFAULT_VALUES expects [" & integer'image(REGISTER_NUMBER) & "] elements")
-		severity failure;
-	--*****************************************************************************************************************
+							    := to_unsigned(BASE_ADDRESS+NUMBER_REGISTERS,BUS_ADDRESS_WIDTH);
+    --Buffers
+    signal reg_buff         :   data_word_vector(NUMBER_REGISTERS-1 downto 0);
 
-	
-	--Output routing
-	reg_data_out <= reg_buff;
-	reg_data_stb <= reg_stb_buff;
-	
-	
-	--! Communication process
-	DATA_TRANSFER : process(clk)
-		--Decoding variables
-		variable reg_index		:	natural;
-		
-	begin
-		if(rising_edge(clk)) then
-			if(rst = '1') then
-				--Reset BUS signals
-				sys_bus_o.dat <= (others => '0');
-				sys_bus_o.err <= '0';
-				
-				--Reset register to default values
-				REG_RESET : for i in 0 to REGISTER_NUMBER-1 loop
-					reg_buff(i) <= REG_DEFAULT_VALUES(i);
-				end loop;
-				reg_stb_buff <= (others => '1');
-				
-				
-			else
-				--Register internal port values independent of BUS operation
-				INTERNAL_PORT : for i in 0 to REGISTER_NUMBER-1 loop
-					if((REG_CONFIGURATION(i) = R) or (REG_CONFIGURATION(i) = BI)) then
-						reg_buff(i) <= reg_data_in(i);
-					end if;
-				end loop;
-				
-				
-				--BUS transaction
-				--Pre-set bus signals 
-				sys_bus_o.dat <= (others => '0');
-				sys_bus_o.err <= '0';
-				reg_stb_buff  <= (others => '0');
-				
-				if( (min_address <= unsigned(sys_bus_i.adr)) and
-					(unsigned(sys_bus_i.adr) < max_address)) then
-					--Decode register
-					reg_index := to_integer(unsigned(sys_bus_i.adr)) - BASE_ADDRESS;
-					
-					--Read/Write operation
-					case sys_bus_i.we is
-						when '1' =>
-							if(REG_CONFIGURATION(reg_index) = R) then
-								sys_bus_o.err <= '1';
-							--Register data but flag stb only if diffrent to avoid constant stb pulses
-							elsif(reg_buff(reg_index) /= sys_bus_i.dat) then
-								reg_buff(reg_index) <=  sys_bus_i.dat;
-								reg_stb_buff(reg_index) <= '1';
-							else
-								reg_buff(reg_index) <=  sys_bus_i.dat;
-								reg_stb_buff(reg_index) <= '0';
-							end if;
-							
-						when '0' =>
-							sys_bus_o.dat <= reg_buff(reg_index);
-						
-						when others => null;
-					end case;
-				else null;
-				end if;
-				
-			end if;
-		end if;
-	end process;
-	
-	
+
+begin
+    --**Synthesis checks**
+    --**
+    assert(BASE_ADDRESS < 2**BUS_ADDRESS_WIDTH)
+        report "Synthesis error - BASE_ADDRESS excedes BUS_ADDRESS bounds" severity failure;
+    assert(NUMBER_REGISTERS < 2**BUS_ADDRESS_WIDTH)
+        report "Synthesis error - Number of registers excedes BUS_ADDRESS bounds" severity failure;
+    assert(BASE_ADDRESS+NUMBER_REGISTERS < 2**BUS_ADDRESS_WIDTH)
+        report "Synthesis error - Selected register addresses out of bounds" severity failure;
+    assert(REG_DEFAULT_VALUES'length = NUMBER_REGISTERS)
+        report "Synthesis error - REG_DEFAULT_VALUES expects " & integer'image(NUMBER_REGISTERS) & " elements" 
+        severity failure;
+    --**
+
+
+
+    BUS_READ_OPERATION : process(clk)
+        variable reg_index  :   natural := 0;
+    begin
+        if(rising_edge(clk)) then
+            if(rst = '1') then
+                sys_bus_o.dat <= (others => '0');
+            
+            elsif((min_address <= unsigned(sys_bus_i.adr)) and
+                  (unsigned(sys_bus_i.adr) < max_address)  and
+                  (sys_bus_i.we = '0')) then
+
+                reg_index := to_integer(unsigned(sys_bus_i.adr))-BASE_ADDRESS;
+                sys_bus_o.dat <= reg_buff(reg_index);
+
+            else
+                reg_index := 0;
+                sys_bus_o.dat <= (others => '0');
+            end if;
+
+        end if;
+    end process;
+
+
+    BUS_WRITE_OPERATION : process(clk)
+        variable reg_index : natural;
+    begin
+        if(rising_edge(clk)) then
+            if(rst = '1') then 
+                --Reset internal port of register table
+                reg_data_out <= REG_DEFAULT_VALUES;
+                --Strobe to indicate reset of registers
+                reg_data_stb <= (others => '1');
+
+            elsif((min_address <= unsigned(sys_bus_i.adr)) and
+                  (unsigned(sys_bus_i.adr) < max_address)  and
+                  (sys_bus_i.we = '1')) then
+                
+                reg_index := to_integer(unsigned(sys_bus_i.adr))-BASE_ADDRESS;
+                reg_data_out(reg_index) <= sys_bus_i.dat;
+                reg_data_stb(reg_index) <= '1';
+            
+            else
+                reg_index := 0;
+                reg_data_stb <= (others => '0');
+            end if;
+        
+        end if;
+    end process;
+
+
+    INTERNAL_READ_OPERATION : process(clk)
+    begin
+        if(rising_edge(clk)) then
+            reg_buff <= reg_data_in;
+        end if;
+    end process;
+
+
 end RTL;
