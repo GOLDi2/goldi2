50fe9b687a10e212c61c18d66ecff7d004f55bda
JP_CC <jose-pablo.chew-camey@tu-ilmenau.de>
2023-01-10 14:47:36 +0100

    V0.01.03.04 - build(src): Incremental encoder dsp
    Unified DSP element for 3 or 2 channel Incremental Encoder.
    Reset mode configurable through generics. 16 bit acummulator
    memory is adaptable to the default word size of the system.

--- hardware/common/fpga/src/dsp/INC_ENCODER.vhd
+++ hardware/common/fpga/src/dsp/INC_ENCODER.vhd
@@ -13,27 +13,39 @@
 --					-> REGISTER_TABLE.vhd
 --
 -- Revisions:
--- Revision V1.00.00 - File Created
+-- Revision V0.01.00 - File Created
 -- Additional Comments: First commitment
+--
+-- Revision V0.01.02 - Changes to register table and code style
+-- Additional Comments: Change of code style to clean the design. Change
+--						to the Register tabel functionality.
+--
+-- Revision V0.01.03 - Modification to BUS convention
+-- Additional Comments: Addition of valid signal to data output and change to 
+--                      naming convention.
+--
+-- Revision V1.00.00 - Default module version for release 1.00.00
+-- Additional Comments: -  
 -------------------------------------------------------------------------------
 --! Use standard library
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 --! Use custom library
-use work.GOLDI_DATA_TYPES.all;
+use work.GOLDI_COMM_STANDARD.all;
+use work.GOLDI_IO_STANDARD.all;
 
 
 
 
---! @brief Incremental encoder driver module 
+--! @brief Incremental encoder dsp module 
 --! @details
 --! Incremental encoder processing unit for a 3 channel sensor.
---! The module reacts to the edges of the a channel providing 2*impulse
---! counts using the b channel to determinate direction. The counter 
---! value is stored in a signed 16 bit integer. The counter values are
---! stored in internal registers and can be access through a custom
---! parallel BUS stucture
+--! The module reacts to the edges of the a channel providing an impulse
+--! counts per channel a edge using the b channel to determinate direction. 
+--! The counter value is stored in a signed 16 bit integer. The counter 
+--! values are stored in internal registers and can be access through a 
+--! custom parallel BUS stucture
 --! 
 --! **Latency:3**
 --!
@@ -45,24 +57,24 @@ use work.GOLDI_DATA_TYPES.all;
 --!
 --! | Address	| Bit 7	| Bit 6 | Bit 5 | Bit 4 | Bit 3 | Bit 2 | Bit 1 | Bit 0 |
 --! |----------:|:-----:|:-----:|:-----:|:-----:|:-----:|:-----:|:-----:|:-----:|
---! | +0		|Enable |       |       |       |       |       |       |rst_mode|
---!	| +1 		| VALUE [7:0] ||||||||
---! | +2		| VALUE [15:8] ||||||||
+--!	| +0 		| VALUE [7:0] ||||||||
+--! | +1		| VALUE [15:8] ||||||||
 entity INC_ENCODER is
 	generic(
-		ADDRESS		:	natural := 1
+		ADDRESS		:	natural := 1;
+		INDEX_RST	:	boolean := false
 	);
 	port(
 		--General
 		clk			: in	std_logic;
 		rst			: in	std_logic;
 		--BUS slave interface
-		sys_bus_i	: in	bus_in;
-		sys_bus_o	: out	bus_out;
+		sys_bus_i	: in	sbus_in;
+		sys_bus_o	: out	sbus_out;
 		--3 Channel encoder signals
-		channel_a	: in	std_logic;
-		channel_b	: in	std_logic;
-		channel_i	: in	std_logic
+		channel_a	: in	io_i;
+		channel_b	: in	io_i;
+		channel_i	: in	io_i
 	);
 end entity INC_ENCODER;
 
@@ -75,33 +87,27 @@ architecture RTL of INC_ENCODER is
 	--Components
 	component REGISTER_TABLE
 		generic(
-			BASE_ADDRESS		:	natural;
-			REGISTER_NUMBER		:	natural;
-			BUS_ADDRESS_WIDTH	:	natural;
-			REG_CONFIGURATION	:	reg_type_array;
-			REG_DEFAULT_VALUES	:	word_8_bit_array
-		);
-		port(
-			clk				: in	std_logic;
-			rst				: in	std_logic;
-			sys_bus_i		: in	bus_in;
-			sys_bus_o		: out	bus_out;
-			reg_data_in		: in	word_8_bit_array(REGISTER_NUMBER-1 downto 0);
-			reg_data_out	: out	word_8_bit_array(REGISTER_NUMBER-1 downto 0);
-			reg_data_stb	: out	std_logic_vector(REGISTER_NUMBER-1 downto 0)
-		);
+		BASE_ADDRESS		:	natural;
+		NUMBER_REGISTERS	:	natural;
+		REG_DEFAULT_VALUES	:	data_word_vector
+	);
+	port(
+		clk				: in	std_logic;
+		rst				: in	std_logic;
+		sys_bus_i		: in	sbus_in;
+		sys_bus_o		: out	sbus_out;
+		reg_data_in		: in	data_word_vector(NUMBER_REGISTERS-1 downto 0);
+		reg_data_out	: out   data_word_vector(NUMBER_REGISTERS-1 downto 0);
+		reg_data_stb	: out	std_logic_vector(NUMBER_REGISTERS-1 downto 0)
+	);
 	end component;
 	
 	
 	--Intermediate signals
 	--Registers
-	signal reg_data_out		:	word_8_bit_array(2 downto 0); 
-		alias enable		:	std_logic is reg_data_out(0)(7);
-		alias rst_mode		:	std_logic is reg_data_out(0)(1);
-	signal reg_data_in		:	word_8_bit_array(2 downto 0);
-		alias low_byte		:	std_logic_vector(7 downto 0) is reg_data_in(1);
-		alias high_byte 	:	std_logic_vector(7 downto 0) is reg_data_in(2);
-	signal reg_data_stb 	:	std_logic_vector(2 downto 0);
+	constant memory_length	:	natural := getMemoryLength(16);
+	constant reg_defaults	:	data_word_vector(memory_length-1 downto 0) := (others => (others => '0'));
+	signal reg_data_in		:	data_word_vector(memory_length-1 downto 0);
 	--Internal decoding
 	signal enc_counter_buff :	std_logic_vector(15 downto 0);
 	signal enc_counter		:	integer := 0;
@@ -114,24 +120,23 @@ begin
 
 	--Output routing
 	enc_counter_buff <= std_logic_vector(to_unsigned(enc_counter,16));
-	low_byte  <= enc_counter_buff(7 downto 0);
-	high_byte <= enc_counter_buff(15 downto 8);
+	reg_data_in <= assignMemory(enc_counter_buff);
 
 
 
 	SIGNAL_DECODE : process(clk)
 	begin
 		if(rising_edge(clk)) then
-			if(rst = '1' or reg_data_stb(0) /= '0') then
+			if(rst = '1') then
 				--Reset internal buffers
 				enc_counter  <= 0;
 				enc_signal_a <= (others => '0');
 				enc_signal_b <= '0';
 				
-			elsif(enc_block = '0' and enable = '1') then
+			elsif(enc_block = '0') then
 				--Buffer signals to detect rising and falling edges
-				enc_signal_a <= enc_signal_a(0) & channel_a;
-				enc_signal_b <= channel_b;
+				enc_signal_a <= enc_signal_a(0) & channel_a.dat;
+				enc_signal_b <= channel_b.dat;
 				
 				case enc_signal_a is
 					when "01" =>
@@ -150,7 +155,6 @@ begin
 						
 					when others => null;
 				end case;
-			
 			else
 				--Ignore encoder movement when encoder blocked or disabled
 				enc_counter  <= 0;
@@ -163,16 +167,17 @@ begin
 	end process;
 	
 	
+	
 	RST_MODE_SELECTION : process(clk)
 	begin
 		if(rising_edge(clk)) then
-			if(rst = '1' and rst_mode = '1') then
+			if((rst = '1') and (INDEX_RST = true)) then
 				enc_block <= '1';
 			
-			elsif(rst = '1' and rst_mode /= '1') then
+			elsif((rst) = '1' and (INDEX_RST = false)) then
 				enc_block <= '0';
 				
-			elsif(enable = '1' and channel_i = '1') then	
+			elsif(channel_i.dat = '1') then	
 				--Unlock incremental encoder with reference pulse i
 				enc_block <= '0';
 		
@@ -188,10 +193,8 @@ begin
 	MEMORY : REGISTER_TABLE
 	generic map(
 		BASE_ADDRESS		=> ADDRESS,
-		REGISTER_NUMBER		=> 3,
-		BUS_ADDRESS_WIDTH	=> BUS_ADDRESS_WIDTH,
-		REG_CONFIGURATION	=> (W,R,R),
-		REG_DEFAULT_VALUES	=> (x"00",x"00",x"00")
+		NUMBER_REGISTERS	=> memory_length,
+		REG_DEFAULT_VALUES	=> reg_defaults
 	)
 	port map(
 		clk				=> clk,
@@ -199,8 +202,8 @@ begin
 		sys_bus_i		=> sys_bus_i,
 		sys_bus_o		=> sys_bus_o,
 		reg_data_in		=> reg_data_in,
-		reg_data_out	=> reg_data_out,
-		reg_data_stb	=> reg_data_stb
+		reg_data_out	=> open,
+		reg_data_stb	=> open
 	);
 	
 	
