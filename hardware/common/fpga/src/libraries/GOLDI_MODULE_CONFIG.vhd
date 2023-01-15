43004bf6a5b21564265710ace70580ad619e4bc8
JP_CC <jose-pablo.chew-camey@tu-ilmenau.de>
2023-01-15 21:19:27 +0100

    V0.01.03.07 - fix(src): Reorganization of GOLDI packages and functions
    Repackaging of some data types and functions into CONFIG package to
    avoid circular referencing. New location for the libraries.

--- hardware/common/fpga/src/libraries/GOLDI_MODULE_CONFIG.vhd
+++ hardware/common/fpga/src/libraries/GOLDI_MODULE_CONFIG.vhd
@@ -3,7 +3,7 @@
 -- Engineer:		JP_CC <josepablo.chew@gmail.com>
 --
 -- Create Date:		15/12/2022
--- Design Name:		Ssytem constants for individual module
+-- Design Name:		System constants for individual module
 -- Module Name:		GOLDI_MODULE_CONFIG
 -- Project Name:	GOLDi_FPGA_CORE
 -- Target Devices:	LCMXO2-7000HC-4TG144C
@@ -26,9 +26,20 @@ use IEEE.numeric_std.all;
 
 package GOLDI_MODULE_CONFIG is
     --System constants
-	constant BUS_ADDRESS_WIDTH	:	natural range 7 to 63 := 7;
-	constant SYSTEM_DATA_WIDTH	:	natural range 8 to 64 := 8;
+	constant BUS_ADDRESS_WIDTH	    :	natural range 7 to 63 := 7;
+	constant SYSTEM_DATA_WIDTH	    :	natural range 8 to 64 := 8;
 
     --Module pins
+    constant PHYSICAL_PIN_NUMBER   	:   natural range 1 to (2**BUS_ADDRESS_WIDTH)-3 := 3;
+    constant VIRTUAL_PIN_NUMBER    	:   natural range 1 to (2**SYSTEM_DATA_WIDTH)-1 := 6;
     
+	--Crossbar data types
+	type phy_io_layout is array(0 to PHYSICAL_PIN_NUMBER-1) of unsigned(SYSTEM_DATA_WIDTH-1 downto 0);
+    type vir_io_layout is array(0 to VIRTUAL_PIN_NUMBER-1) of unsigned(BUS_ADDRESS_WIDTH-1 downto 0);
+	
+    constant DEFAULT_IO_LAYOUT      :   phy_io_layout :=
+        (0 => x"00",
+         1 => x"01",
+         2 => x"02");
+  
 end package;
