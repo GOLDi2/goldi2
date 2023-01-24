98fb3eec33b9205e216027db51a4142d6b2e7e1d
Johannes Nau <johannes.nau@outlook.com>
2023-01-24 17:46:42 +0000

    ci / cd integration

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -4,4 +4,4 @@ REPOSITORY=admin@ci.goldi-labs.de:/data/www/ci/goldi
 WEB_REPOSITORY=https://ci.goldi-labs.de/goldi
 
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
-$SCRIPT_DIR/helper/ci.sh --repository $REPOSITORY --web-repository $WEB_REPOSITORY $@
+$SCRIPT_DIR/../crosslab/scripts/ci.sh --skip-upload $@ && $SCRIPT_DIR/helper/ci.sh --repository $REPOSITORY --web-repository $WEB_REPOSITORY $@
