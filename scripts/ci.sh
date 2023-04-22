c12b69581c6937ec0c7357617817775c583cf09e
Johannes Nau <johannes.nau@outlook.com>
2023-04-22 17:26:45 +0000

    fix

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -4,5 +4,5 @@ SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 REPOSITORY=admin@ci.goldi-labs.de:/data/www/ci/goldi
 WEB_REPOSITORY=https://ci.goldi-labs.de/goldi
 
-#$SCRIPT_DIR/../crosslab/scripts/ci.sh $@ --no-upload
+$SCRIPT_DIR/../crosslab/scripts/ci.sh $@ --no-upload
 $SCRIPT_DIR/ci.d/ci.sh --include crosslab --repository $REPOSITORY --web-repository $WEB_REPOSITORY $@
