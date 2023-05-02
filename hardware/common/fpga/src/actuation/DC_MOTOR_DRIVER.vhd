c946c637ff6fe3a2ed115e4e792fc16b7812592b
JP_CC <jose-pablo.chew-camey@tu-ilmenau.de>
2023-05-02 21:56:28 +0200

    C01.02.03 - fix(src): Intergration of new memory units
    Correction of actuators based on the introduction of the new
    memory units REGISTER_UNIT / REGISTER_TABLE to solve register
    indexing problems.

--- hardware/common/fpga/src/actuation/DC_MOTOR_DRIVER.vhd
+++ hardware/common/fpga/src/actuation/DC_MOTOR_DRIVER.vhd
@@ -167,9 +167,10 @@ begin
 		rst					=> rst,
 		sys_bus_i			=> sys_bus_i,
 		sys_bus_o			=> sys_bus_o,
-		reg_data_in			=> reg_data,
-		reg_data_out		=> reg_data,
-		reg_data_stb		=> reg_data_stb
+		data_in				=> reg_data,
+		data_out			=> reg_data,
+		read_stb			=> open,
+		write_stb			=> reg_data_stb
 	);
 	-----------------------------------------------------------------------------------------------
 
