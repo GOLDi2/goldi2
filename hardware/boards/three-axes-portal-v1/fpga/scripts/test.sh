7e75b889b9fd987f91c773181186b35d47de5783
Johannes Nau <johannes.nau@outlook.com>
2023-05-03 13:05:48 +0000

    remove old board files

--- hardware/boards/three-axes-portal-v1/fpga/scripts/test.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p .ghdl
-
-docker run -it \
-  -v $(pwd)/scripts:/project/scripts \
-  -v $(pwd):/project/1/2/3/ \
-  -v $(pwd)/../../../common/fpga/:/project/common/fpga/ \
-  ghdl/ghdl:ubuntu20-llvm-10 /project/scripts/.test.sh
--- hardware/boards/three-axes-portal-v1/fpga/scripts/upload.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-IP=169.254.79.79
-
-sshpass -p $PASSWORD scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null dist/bitstream.svf root@${IP}:/lib/firmware/lattice/firmware.svf
-sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${IP} "systemctl start load-fpga-firmware.service"
