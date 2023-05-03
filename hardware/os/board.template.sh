71b69ed5e5af9d3aada1ddeba2f7b1b295e92faa
Johannes Nau <johannes.nau@outlook.com>
2023-05-03 07:51:24 +0000

    bugfixing

--- hardware/os/board.template.sh
+++
@@ -1,6 +0,0 @@
-BOARD=<board name, e.g. microcontroller>
-IP=<Ip address of the board>
-PASSWORD=<Password of the board>
-VPN_CONFIG=<Private VPN Config>
-
-# You can then source board.sh to get the variables for the scripts.
