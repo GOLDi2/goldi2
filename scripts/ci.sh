f4347c9e2e5ff95f16670dc71a33f637b305fd38
Johannes Nau <johannes.nau@outlook.com>
2023-05-05 09:01:41 +0000

    exit when build fails

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -25,6 +25,8 @@ REPOSITORY=admin@ci.goldi-labs.de:/data/www/ci/goldi
 WEB_REPOSITORY=https://ci.goldi-labs.de/goldi
 
 $SCRIPT_DIR/../crosslab/scripts/ci.sh $SUBCOMMANDVARS --no-upload
+
+set -e
 $SCRIPT_DIR/ci.d/ci.sh --include crosslab --repository $REPOSITORY --web-repository $WEB_REPOSITORY $SUBCOMMANDVARS
 
 if [ -n "$DEPLOY" ]; then
