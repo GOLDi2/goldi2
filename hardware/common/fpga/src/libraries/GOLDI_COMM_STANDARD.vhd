4c43a3de8c000983768e5811bcd6c15567a3a997
JP_CC <jose-pablo.chew-camey@tu-ilmenau.de>
2023-05-08 19:20:41 +0200

    C02.01.04 - fix(src): Correction of REGISTER modules instantiation and use
    Correction of REGISTER_TABLE entity and introduction of REGISTER_UNIT
    module for simple registers. Correction of reg_default_value assignment.

--- hardware/common/fpga/src/libraries/GOLDI_COMM_STANDARD.vhd
+++ hardware/common/fpga/src/libraries/GOLDI_COMM_STANDARD.vhd
@@ -4,12 +4,12 @@
 --
 -- Create Date:		15/04/2023
 -- Design Name:		Custom communication data types for Goldi_FPGA_SRC project
--- Module Name:		GOLDI_BUS_STANDARD
+-- Module Name:		GOLDI_COMM_STANDARD
 -- Project Name:	GOLDi_FPGA_SRC
 -- Target Devices:	LCMXO2-7000HC-4TG144C
 -- Tool versions:	Lattice Diamond 3.12, Modelsim Lattice Edition 
 --
--- Dependencies: 	-> GOLDI_MODULE_CONFIG.vhd
+-- Dependencies: 	none
 --
 -- Revisions:
 -- Revision V0.01.00 - File Created
@@ -114,6 +114,7 @@ package GOLDI_COMM_STANDARD is
     -----------------------------------------------------------------------------------------------
     --Synthesisable functions
     function reduceBusVector(bus_vector : sbus_o_vector) return sbus_out;
+    function reduceRegStrobe(stb_vector : std_logic_vector) return std_logic;
     function getMemoryLength(vector_length : natural) return natural;
     function setMemory(data_vector : std_logic_vector) return data_word_vector;
     function getMemory(data_vector : data_word_vector) return std_logic_vector;
@@ -135,8 +136,10 @@ package body GOLDI_COMM_STANDARD is
 
     --****SYNTHESIS FUNCTIONS****
     -----------------------------------------------------------------------------------------------
-    -- Returns a sbus_out structure corresponding to the addressed register.
-	-- Used in synthesis to generate multiplexer for multiple register tables.
+    --! @brief BUS vector "sbus_out" multiplexer 
+    --! @details
+    --! Returns a sbus_out structure corresponding to the addressed register.
+	--! Used in synthesis to generate multiplexer for multiple register tables.
     function reduceBusVector(bus_vector : sbus_o_vector) return sbus_out is
     begin
         for i in bus_vector'range loop
@@ -150,8 +153,25 @@ package body GOLDI_COMM_STANDARD is
 
 
 
-    -- Returns the minimum number of registers needed to save a vector of  a given 
-    -- size; based on the SYSTEM_DATA_WIDTH of the GOLDI_MODULE_CONFIG package.
+    --! @brief Strobe vector reduction
+    --! @details
+    --! Function reduces the strobe vector of the REGISTER_TABLE to a  
+    --! std_logic signal equivalent to the "or" operation
+    function reduceRegStrobe(stb_vector : std_logic_vector) return std_logic is
+    begin
+        if((stb_vector'range => '0') = stb_vector) then
+            return '0';
+        else
+            return '1';
+        end if;
+    end function;
+
+
+
+    --! @brief Get number of registers for a std_logic_vector range
+    --! @details 
+    --! Returns the minimum number of registers needed to save a vector of  a given 
+    --! size; based on the SYSTEM_DATA_WIDTH of the GOLDI_MODULE_CONFIG package.
     function getMemoryLength(vector_length : natural) return natural is
         variable quotient   :   natural;
         variable rest       :   natural;
@@ -167,11 +187,12 @@ package body GOLDI_COMM_STANDARD is
     end function;
 
 
-
-    -- Returns a data_word_vector corresponding to the minimum number
-	-- of register to save "data". The index 0 of the logic_vector is taken
-	-- as the lowest index of the register 0 and "data" is assigned in ascending
-	-- order.
+    --! @brief Convert std_logic_vector to data_word_vector
+    --! @details
+    --! Returns a data_word_vector corresponding to the minimum number
+	--! of register to save "data". The index 0 of the logic_vector is taken
+	--! as the lowest index of the register 0 and "data" is assigned in ascending
+	--! order.
     function setMemory(data_vector : std_logic_vector) return data_word_vector is
         variable memory         :   data_word_vector(getMemoryLength(data_vector'length)-1 downto 0);
         variable data_buff      :   std_logic_vector((getMemoryLength(data_vector'length)*SYSTEM_DATA_WIDTH)-1 downto 0);
@@ -198,8 +219,10 @@ package body GOLDI_COMM_STANDARD is
 
 
 
-    -- Function converts a subset of registers into a std_logic_vector
-	-- Use for vector divided in multiple registers.
+    --! @brief Convert data_word_vector to std_logic_vector
+    --! @details
+    --! Function converts a subset of registers into a std_logic_vector
+	--! Use for vector divided in multiple registers.
     function getMemory(data_vector : data_word_vector) return std_logic_vector is
 		variable vector : std_logic_vector((data_vector'length*SYSTEM_DATA_WIDTH)-1 downto 0);
   	begin
@@ -212,8 +235,10 @@ package body GOLDI_COMM_STANDARD is
 
 
 
-    -- Function used to invert the index of a std_logic_vector in case of
-    -- "to" use instead of standard "downto"
+    --! @brief Invert index "to" to "downto" <-> "downto" to "to"
+    --! @details
+    --! Function used to invert the index of a std_logic_vector in case of
+    --! "to" use instead of standard "downto"
     function invertIndex(data_vector : std_logic_vector) return std_logic_vector is
         variable data_buff : std_logic_vector(data_vector'reverse_range);
     begin
@@ -230,8 +255,10 @@ package body GOLDI_COMM_STANDARD is
 
     --****SIMULATION FUNCTIONS****
     -----------------------------------------------------------------------------------------------
-    -- Function used for simulation. Converts std_logic_vector into 
-    -- master bus input configured for read operation
+    --! @brief Configure BUS to read register
+    --! @details
+    --! Function used for simulation. Converts std_logic_vector into 
+    --! master bus input configured for read operation
     function readBus(adr : std_logic_vector) return mbus_out is
         variable adr_buff   :   std_logic_vector(adr'length-1 downto 0);
         variable sys_bus    :   mbus_out;
@@ -254,8 +281,10 @@ package body GOLDI_COMM_STANDARD is
 
 
 
-    -- Function used for simulation. Converts natural integer into 
-    -- master bus input configured for read operation
+    --! @brief Configure BUS to read register
+    --! @details
+    --! Function used for simulation. Converts natural integer into 
+    --! master bus input configured for read operation
     function readBus(adr : natural) return mbus_out is
         variable sys_bus : mbus_out;
     begin
@@ -267,6 +296,8 @@ package body GOLDI_COMM_STANDARD is
 
 
 
+    --! @brief Configure BUS to write register
+    --! @details
     -- Function used for simulation. Converts std_logic_vector into 
     -- master bus input configured for read operation
     function writeBus(adr : std_logic_vector; dat : std_logic_vector) return mbus_out is
@@ -298,6 +329,8 @@ package body GOLDI_COMM_STANDARD is
 
 
 
+    --! @brief Configure BUS to write register
+    --! @details
     -- Function used for simulation. Converts natural integer into 
     -- master bus input configured for read operation
     function writeBus(adr : natural; dat : natural) return mbus_out is
