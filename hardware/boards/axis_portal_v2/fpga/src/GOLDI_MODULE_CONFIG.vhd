979960dfe173da2bc62b08b9edc880f5ee9f81b0
JP_CC <jose-pablo.chew-camey@tu-ilmenau.de>
2023-05-27 11:57:50 +0200

    C03.00.01 - build(src): Inclussion of new Electromagnet module
    New electromagnet module with pulsing depolarization. Inclusion
    of the new constants in the GOLDI_MODULE_CONFIG package

--- hardware/boards/axis_portal_v2/fpga/src/GOLDI_MODULE_CONFIG.vhd
+++ hardware/boards/axis_portal_v2/fpga/src/GOLDI_MODULE_CONFIG.vhd
@@ -92,7 +92,9 @@ package GOLDI_MODULE_CONFIG is
     constant X_MOTOR_SCLK_FACTOR    :   natural := 48;
 
     --Initial configuration of the TMC2660 Stepper driver
-    constant X_MOTOR_CONFIGURATION  :   tmc2660_rom(4 downto 0) :=(
+    constant X_MOTOR_CONFIGURATION  :   tmc2660_rom(5 downto 0) :=(
+        0 => x"000000",
+
         --**Driver Control Register STEP/DIR mode (DRVCTRL)**
         --[19:18]   Address = 00
         --[17:10]   Reserved -> '0'
@@ -100,7 +102,7 @@ package GOLDI_MODULE_CONFIG is
         --[8]       Enable double edge STEP pulses
         --[7:4]     Reserved -> '0'
         --[3:0]     Microstep resolution for STEP/DIR mode
-        0 => x"000004",   --x"00004"
+        1 => x"000004",   --x"00004"
 
         --**Chopper Control Register (CHOPCONF)**
         --[19:17]   Address = 100
@@ -111,7 +113,7 @@ package GOLDI_MODULE_CONFIG is
         --[10:7]    Hysteresis end value (low)
         --[6:4]     Hysteresis start value 
         --[3:0]     Off time MOSFET disable  
-        1 => x"094557",   --x"94557"
+        2 => x"094557",   --x"94557"
       
         --**Coolstep Control Register (SMARTEN)**
         --[19:17]   Address = 101
@@ -124,7 +126,7 @@ package GOLDI_MODULE_CONFIG is
         --[6:5]     Current increment size
         --[4]       Reserved -> '0'
         --[3:0]     Lower coolStep threshold SEMIN
-        2 => x"0A0000",   --x"A0000" --CoolStep disabled[SEMIN=0]
+        3 => x"0A0000",   --x"A0000" --CoolStep disabled[SEMIN=0]
 
         --**StallGuard2 Control Register (SGCSCONF)**
         --[19:17]   Address = 110
@@ -133,7 +135,7 @@ package GOLDI_MODULE_CONFIG is
         --[14:8]    StallGuard2 threshold value
         --[7:5]     Reserved -> '0'
         --[4:0]     Current scale  
-        3 => x"0C040F",   --x"C041E"
+        4 => x"0C040F",   --x"C040F"
         
         --**Driver Control Register (DRVCONF)**
         --[19:17]   Address = 111
@@ -147,7 +149,7 @@ package GOLDI_MODULE_CONFIG is
         --[6]       Sense resistor voltage-based current scaling
         --[5:4]     Select value for read out
         --[3:0]     Reserved -> '0'
-        4 => x"0E0070" --x"E0070"
+        5 => x"0E0070" --x"E0070"
     );
     -----------------------------------------------------------------------------------------------
 
@@ -160,7 +162,9 @@ package GOLDI_MODULE_CONFIG is
     constant Y_MOTOR_SCLK_FACTOR    :   natural := 48;
 
     --Initial configuration of the TMC2660 Stepper driver
-    constant Y_MOTOR_CONFIGURATION  :   tmc2660_rom(4 downto 0) :=(
+    constant Y_MOTOR_CONFIGURATION  :   tmc2660_rom(5 downto 0) :=(
+        0 => x"000000",
+
         --**Driver Control Register STEP/DIR mode (DRVCTRL)**
         --[19:18]   Address = 00
         --[17:10]   Reserved -> '0'
@@ -168,7 +172,7 @@ package GOLDI_MODULE_CONFIG is
         --[8]       Enable double edge STEP pulses
         --[7:4]     Reserved -> '0'
         --[3:0]     Microstep resolution for STEP/DIR mode
-        0 => x"000004",   --x"00004"
+        1 => x"000004",   --x"00004"
 
         --**Chopper Control Register (CHOPCONF)**
         --[19:17]   Address = 100
@@ -179,7 +183,7 @@ package GOLDI_MODULE_CONFIG is
         --[10:7]    Hysteresis end value (low)
         --[6:4]     Hysteresis start value 
         --[3:0]     Off time MOSFET disable  
-        1 => x"094557",   --x"94557"
+        2 => x"094557",   --x"94557"
       
         --**Coolstep Control Register (SMARTEN)**
         --[19:17]   Address = 101
@@ -192,7 +196,7 @@ package GOLDI_MODULE_CONFIG is
         --[6:5]     Current increment size
         --[4]       Reserved -> '0'
         --[3:0]     Lower coolStep threshold SEMIN
-        2 => x"0A0000",   --x"A0000" --CoolStep disabled[SEMIN=0]
+        3 => x"0A0000",   --x"A0000" --CoolStep disabled[SEMIN=0]
 
         --**StallGuard2 Control Register (SGCSCONF)**
         --[19:17]   Address = 110
@@ -201,7 +205,7 @@ package GOLDI_MODULE_CONFIG is
         --[14:8]    StallGuard2 threshold value
         --[7:5]     Reserved -> '0'
         --[4:0]     Current scale  
-        3 => x"0C040F",   --x"C040F"
+        4 => x"0C040F",   --x"C040F"
         
         --**Driver Control Register (DRVCONF)**
         --[19:17]   Address = 111
@@ -215,7 +219,7 @@ package GOLDI_MODULE_CONFIG is
         --[6]       Sense resistor voltage-based current scaling
         --[5:4]     Select value for read out
         --[3:0]     Reserved -> '0'
-        4 => x"0E0070" --x"E0070"
+        5 => x"0E0070" --x"E0070"
     );
     -----------------------------------------------------------------------------------------------
 
@@ -237,11 +241,16 @@ package GOLDI_MODULE_CONFIG is
     -----------------------------------------------------------------------------------------------
     --Electromagnet time constant in clk cycles. Used as a wait time to avoid an imediate voltage 
     --drop on the unprotected H-Bridge inputs when the depolarization pulse in generated.
-    constant EMAG_TAO           :   natural := 12000;
+    constant EMAG_TAO               :   natural := 5000;
 
     --Depolarization pulse duration in clk cycles. Used to reduce remanent polarization when the
     --magnet is powered off. To disable function use a value of 0
-    constant EMAG_DEMAG_FACTOR  :   natural := 1000;
+    constant EMAG_DEMAG_FACTOR      :   natural := 50000;
+
+    --Pulse reduction constant. Demagnetization pulse reduced from the starting value in the 
+    --register (reg_data*1000) by the given factor. Signal returns to idel when the pulse
+    --width is smaller than the reduction factor
+    constant EMAG_PULSE_REDUCTION   :   integer := 5000;
     -----------------------------------------------------------------------------------------------
 
 
