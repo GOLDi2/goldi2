61c345a33464b76194327efcbecf2711a1608979
JP_CC <jose-pablo.chew-camey@tu-ilmenau.de>
2023-06-30 10:34:12 +0200

    C07.00.03 - build(src): Testbenches for CU_V1
    Creation of a MOLE to simulate entire CU_V1

--- hardware/boards/warehouse_v2/fpga/src/ACTUATOR_MASK.vhd
+++ hardware/boards/warehouse_v2/fpga/src/ACTUATOR_MASK.vhd
@@ -1,5 +1,5 @@
 -------------------------------------------------------------------------------
--- Company:			Technische Universität Ilmenau
+-- Company:			Technische Universitaet Ilmenau
 -- Engineer:		JP_CC <josepablo.chew@gmail.com>
 --
 -- Create Date:		30/04/2023
