1a2429c6856527a8858e67d8ed304d06e35536f3
JP_CC <jose-pablo.chew-camey@tu-ilmenau.de>
2023-09-12 13:34:30 +0200

    C01.02.12 - refactor(EMAG_2): Change to the pulse_width paramter
    Magnet module uses a static g_puse_width parameter instead of data
    stored in a register

--- hardware/common/fpga/src/actuation/EMAGNET_SMODULE_2.vhd
+++ hardware/common/fpga/src/actuation/EMAGNET_SMODULE_2.vhd
@@ -11,7 +11,7 @@
 --
 -- Dependencies:	-> GOLDI_COMM_STANDARD.vhd
 --                  -> GOLDI_IO_STANDARD.vhd
---                  -> EMAGNET_DRIVER.vhd
+--                  -> REGISTER_UNIT.vhd
 --
 -- Revisions:
 -- Revision V3.00.01 - File Created
@@ -19,6 +19,11 @@
 --
 -- Revision V3.00.01 - First release
 -- Additional Comments: -
+--
+-- Revision V4.00.00 - Module renaming and refactor
+-- Additional Comments: Renaming of module to follow V4.00.00 conventions.
+--                      (EMAGNET_DRIVER_2.vhd -> EMAGNET_SMODULE_2.vhd)
+--                      Pulse width modified to static parameter.
 -------------------------------------------------------------------------------
 --! Use standard library
 library IEEE;
@@ -32,46 +37,64 @@ use work.GOLDI_IO_STANDARD.all;
 
 
 
---! @brief
+--! @brief Electromagnet driver module using an H-Bridge
 --! @details
+--! H-Bridge driver for control of an electromagnet. The module is designed
+--! for a double channel (Out_1 and Out_2) configuration to reduece the magnetic
+--! remanence effects. This is achieved through decreasing alternating pulses
+--! that reduce the hysteresis effect. The inital pulse width is set by the
+--! parameter "g_pulse_width" and the width reduction is set by the "g_pulse_reduction"
+--! factors. The pulse width can be calculated as clk_period*g_pulse_width
 --!
-entity EMAGNET_DRIVER_2 is
+--! To prevent a shortcut or overload of the H-Bridge the module first waits for
+--! the current to decrease to limit the inductive effects. The waiting time is
+--! set by the "g_magnet_tao" parameter. This parameter also acts as the initial
+--! width of the demagnetizatio pulses.
+--!
+--! #### Register:
+--! | g_address | Bit 7 | Bit 6 | Bit 5 | Bit 4 | Bit 3 | Bit 2 | Bit 1 | Bit 0 |
+--! | +0		|		|		|		|		|		|		|		|EM_pow	|
+--!
+--! **Latency: 3 **
+entity EMAGNET_SMODULE_2 is
     generic(
-        ADDRESS         :   integer := 1;
-        MAGNET_TAO      :   integer := 10;
-        PULSE_REDUCTION :   integer := 10
+        g_address           :   integer := 1;       --! Module's base address
+        g_magnet_tao        :   integer := 10;      --! Electromagnet time constant
+        g_pulse_width       :   integer := 100;     --! Demagnetization pulse inital width
+        g_pulse_reduction   :   integer := 10       --! Demagnetization pulse width reduction
     );
     port(
-        clk             : in    std_logic;
-        rst             : in    std_logic;
-        sys_bus_i       : in    sbus_in;
-        sys_bus_o       : out   sbus_out;
-        emag_enb        : out   io_o;
-        emag_out_1      : out   io_o;
-        emag_out_2      : out   io_o
+        --General
+        clk                 : in    std_logic;      --! System clock
+        rst                 : in    std_logic;      --! Asynchronous reset
+        --BUS interface
+        sys_bus_i           : in    sbus_in;        --! BUS input signals [stb,we,adr,dat,tag]
+        sys_bus_o           : out   sbus_out;       --! BUS output signals [dat,tag]
+        --HBridge interface
+        p_em_enb            : out   io_o;           --! H-Bridge Enale
+        p_em_out_1          : out   io_o;           --! H-Bridge Output 1
+        p_em_out_2          : out   io_o            --! H-Bridge Output 2
     );
-end entity EMAGNET_DRIVER_2;
+end entity EMAGNET_SMODULE_2;
 
 
 
 
 --! General architecture
-architecture RTL of EMAGNET_DRIVER_2 is
+architecture RTL of EMAGNET_SMODULE_2 is
 
     --****INTERNAL SIGNALS****
     --Memory
-    constant reg_default    :   data_word := (others => '0');
-    signal reg_data         :   data_word;
-        alias power         :   std_logic is reg_data(7);
-        alias pulse_factor  :   std_logic_vector(6 downto 0) is reg_data(6 downto 0);  
+    constant c_reg_default      :   data_word := (others => '0');
+    signal reg_data             :   data_word;
     --Data buffers
-    signal pulse_width      :   integer;
+    signal pulse_width          :   integer;
     --Counters
-    signal tao_counter      :   integer range 0 to MAGNET_TAO;
-    signal pulse_counter    :   integer;
+    signal tao_counter          :   integer range 0 to g_magnet_tao;
+    signal pulse_counter        :   integer range 0 to g_pulse_width;
     --State machines
-    type magnet_state is (IDLE, POWER_ON, POWER_OFF);
-    type pulse_state is (HOLD_TAO, PULSE_NEG, PULSE_POS);
+    type magnet_state is (s_idle, s_power_on, s_power_off);
+    type pulse_state is (s_hold_tao, s_pulse_pos, s_pulse_neg);
     signal ps_magnet    :   magnet_state;
     signal ps_pulse     :   pulse_state;
     signal ns_pulse     :   pulse_state;
@@ -84,20 +107,24 @@ begin
     MAGNET_CONTROL : process(clk,rst)
     begin
         if(rst = '1') then
-            ps_magnet <= POWER_OFF;
+            ps_magnet <= s_idle;
+
         elsif(rising_edge(clk)) then
             --Magnet state machine
             case ps_magnet is
-            when IDLE =>        if(power = '1') then ps_magnet <= POWER_ON;  
-                                else ps_magnet <= IDLE;
+            when s_idle =>      if(reg_data(0) = '1') then ps_magnet <= s_power_on;
+                                else ps_magnet <= s_idle;
                                 end if;
-            when POWER_ON =>    if(power = '0') then ps_magnet <= POWER_OFF; 
-                                else ps_magnet <= POWER_ON;
+            
+            when s_power_on =>  if(reg_data(0) = '0') then ps_magnet <= s_power_off;
+                                else ps_magnet <= s_power_on;
                                 end if;
-            when POWER_OFF =>   if(pulse_width < PULSE_REDUCTION) then ps_magnet <= IDLE;  
-                                else ps_magnet <= POWER_OFF;
+
+            when s_power_off => if(pulse_width < g_pulse_reduction) then ps_magnet <= s_idle;
+                                else ps_magnet <= s_power_off;
                                 end if;
-            when others =>  ps_magnet <= IDLE;
+
+            when others => ps_magnet <= s_idle;
             end case;
         end if;
     end process;
@@ -106,32 +133,37 @@ begin
     PULSE_CONTROL : process(clk,rst)
     begin
         if(rst = '1') then
-            ps_pulse    <= HOLD_TAO;
-            ns_pulse    <= PULSE_NEG;
-            pulse_width <= 0;
+            ps_pulse    <= s_hold_tao;
+            ns_pulse    <= s_pulse_neg;
+            pulse_width <= g_pulse_width;
+        
         elsif(rising_edge(clk)) then
-            if(ps_magnet /= POWER_OFF) then
-                ps_pulse    <= HOLD_TAO;
-                ns_pulse    <= PULSE_NEG;
-                pulse_width <= 1000*to_integer(unsigned(pulse_factor));
+            if(ps_magnet /= s_power_off) then
+                ps_pulse    <= s_hold_tao;
+                ns_pulse    <= s_pulse_neg;
+                pulse_width <= g_pulse_width;
+            
             else
                 --Control pulse generation
-                case ps_pulse is
-                when HOLD_TAO =>    if(tao_counter = MAGNET_TAO-1) then ps_pulse <= ns_pulse;
-                                    else ps_pulse <= HOLD_TAO;
+               case ps_pulse is
+                when s_hold_tao =>  if(tao_counter = g_magnet_tao-1) then ps_pulse <= ns_pulse;
+                                    else ps_pulse <= s_hold_tao;
                                     end if;
-                when PULSE_NEG =>   if(pulse_counter = pulse_width-1) then
-                                        ps_pulse <= HOLD_TAO;
-                                        ns_pulse <= PULSE_POS;
-                                        pulse_width <= pulse_width - PULSE_REDUCTION;
-                                    else ps_pulse <= PULSE_NEG;
+                
+                when s_pulse_neg => if(pulse_counter = pulse_width-1) then
+                                        ps_pulse    <= s_hold_tao;
+                                        ns_pulse    <= s_pulse_pos;
+                                        pulse_width <= pulse_width - g_pulse_reduction;
+                                    else ps_pulse <= s_pulse_neg;
                                     end if;
-                when PULSE_POS =>   if(pulse_counter = pulse_width-1) then
-                                        ps_pulse <= HOLD_TAO;
-                                        ns_pulse <= PULSE_NEG;
-                                    else ps_pulse <= PULSE_POS;
+                
+                when s_pulse_pos => if(pulse_counter = pulse_width-1) then
+                                        ps_pulse <= s_hold_tao;
+                                        ns_pulse <= s_pulse_neg;
+                                    else ps_pulse <= s_pulse_pos;
                                     end if;
-                when others =>      ps_pulse <= HOLD_TAO;
+                                    
+                when others =>      ps_pulse <= s_hold_tao;
                 end case;
             end if;
         end if;
@@ -139,17 +171,16 @@ begin
     -----------------------------------------------------------------------------------------------
 
 
-    
-    --****COUNTERS****
+--****COUNTERS****
     -----------------------------------------------------------------------------------------------
     PULSE_WIDTH_GENERATION : process(clk,rst)
     begin
         if(rst = '1') then
             pulse_counter <= 0;
         elsif(rising_edge(clk)) then
-            if(ps_magnet /= POWER_OFF) then
+            if(ps_magnet /= s_power_off) then
                 pulse_counter <= 0;
-            elsif(ps_magnet = POWER_OFF and ps_pulse = HOLD_TAO) then
+            elsif(ps_magnet = s_power_off and ps_pulse = s_hold_tao) then
                 pulse_counter <= 0;
             elsif(pulse_counter = pulse_width-1) then
                 pulse_counter <= 0;
@@ -166,11 +197,11 @@ begin
             tao_counter <= 0;
         elsif(rising_edge(clk)) then
             --Reset counter as long as the magnet is not in power off mode
-            if(ps_magnet /= POWER_OFF) then
+            if(ps_magnet /= s_power_off) then
                 tao_counter <= 0;
-            elsif(ps_pulse /= HOLD_TAO) then
+            elsif(ps_pulse /= s_hold_tao) then
                 tao_counter <= 0;
-            elsif(tao_counter = MAGNET_TAO-1) then
+            elsif(tao_counter = g_magnet_tao-1) then
                 tao_counter <= 0;
             else 
                 tao_counter <= tao_counter + 1;
@@ -184,22 +215,22 @@ begin
     --****H-BRIDGE SIGNALS****
     -----------------------------------------------------------------------------------------------
     --Enable signal for the H-Bridge
-    emag_enb.enb <= '1';
-    emag_enb.dat <= '1' when((ps_magnet = POWER_ON)                             or 
-                             (ps_magnet = POWER_OFF and ps_pulse = PULSE_NEG)   or
-                             (ps_magnet = POWER_OFF and ps_pulse = PULSE_POS))  else
+    p_em_enb.enb <= '1';
+    p_em_enb.dat <= '1' when((ps_magnet = s_power_on)                               or 
+                             (ps_magnet = s_power_off and ps_pulse = s_pulse_neg)   or
+                             (ps_magnet = s_power_off and ps_pulse = s_pulse_pos))  else
                     '0';
 
     --Out 1 for H-Bridge. Main power signal for electromagnet
-    emag_out_1.enb <= '1';
-    emag_out_1.dat <=   '1' when((ps_magnet = POWER_ON)                             or
-                                 (ps_magnet = POWER_OFF and ps_pulse = PULSE_POS))  else
+    p_em_out_1.enb <= '1';
+    p_em_out_1.dat <= '1' when((ps_magnet = s_power_on)                               or
+                               (ps_magnet = s_power_off and ps_pulse = s_pulse_pos))  else
                         '0';
 
     --Out 2 for H-Bridge. Demagnetization line
-    emag_out_2.enb <= '1';
-    emag_out_2.dat <=   '1' when((ps_magnet = POWER_OFF and ps_pulse = PULSE_NEG))  else
-                        '0';
+    p_em_out_2.enb <= '1';
+    p_em_out_2.dat <= '1' when((ps_magnet = s_power_off and ps_pulse = s_pulse_neg))  else
+                      '0';
     -----------------------------------------------------------------------------------------------
 
 
@@ -208,8 +239,8 @@ begin
     -----------------------------------------------------------------------------------------------
     MEMORY : entity work.REGISTER_UNIT
     generic map(
-        ADDRESS     => ADDRESS,
-        DEF_VALUE   => reg_default
+        ADDRESS     => g_address,
+        DEF_VALUE   => c_reg_default
     )
     port map(
         clk         => clk,
@@ -224,4 +255,4 @@ begin
     -----------------------------------------------------------------------------------------------
 
 
-end RTL;
+end architecture;
--- hardware/common/fpga/tests/actuation/EMAGNET_SMODULE_2_TB.vhd
+++ hardware/common/fpga/tests/actuation/EMAGNET_SMODULE_2_TB.vhd
@@ -4,98 +4,104 @@
 --
 -- Create Date:		26/05/2023
 -- Design Name:		Electromagnet driver testbench
--- Module Name:		EMAGNET_DRIVER_TB
+-- Module Name:		EMAGNET_SMODULE_2_TB
 -- Project Name:	GOLDi_FPGA_SRC
 -- Target Devices:	LCMXO2-7000HC-4TG144C
 -- Tool versions:	Lattice Diamond 3.12, Modelsim Lattice Edition,  
 --
 -- Dependencies:	-> GOLDI_COMM_STANDARD.vhd
 --                  -> GOLDI_IO_STANDARD.vhd
---                  -> EMAGNET_DRIVER.vhd
+--                  -> EMAGNET_SMODULE_2.vhd
 --
 -- Revisions:
 -- Revision V3.00.01 - File Created
 -- Additional Comments: First commitment
 --
--- Revision V3.01.00 - First release
--- Additional Comments: -
+-- Revision V4.00.00 - Module renaming and refactoring
+-- Additional Comments: Module renamed to follow V4.00.00 naming convention.
+--                      (EMAGNET_DRIVER_TB.vhd -> EMAGNET_SMODULE_TB.vhd)
+--						Changes to the DUT entity and the port signal names.
+--						Use of env library to stop simulation.
 -------------------------------------------------------------------------------
 --! Use standard library
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
+--! Use standard library for simulation flow control and assertions
+library std;
+use std.standard.all;
+-- use std.env.all;
 --! Use custom packages
 library work;
 use work.GOLDI_COMM_STANDARD.all;
 use work.GOLDI_IO_STANDARD.all;
---! Use assert library for simulation
-library std;
-use std.standard.all;
 
 
 
 
 --! Functionality simulation
-entity EMAGNET_DRIVER_2_TB is
-end entity EMAGNET_DRIVER_2_TB;
+entity EMAGNET_SMODULE_2_TB is
+end entity EMAGNET_SMODULE_2_TB;
 
 
 
 
 --! Simulation architecture
-architecture TB of EMAGNET_DRIVER_2_TB is
+architecture TB of EMAGNET_SMODULE_2_TB is
 
     --****DUT****
-    component EMAGNET_DRIVER_2
+    component EMAGNET_SMODULE_2
         generic(
-            ADDRESS         :   integer := 1;
-            MAGNET_TAO      :   integer := 10;
-            PULSE_REDUCTION :   integer := 10
+            g_address           :   integer;
+            g_magnet_tao        :   integer;
+            g_pulse_width       :   integer;
+            g_pulse_reduction   :   integer
         );
         port(
-            clk             : in    std_logic;
-            rst             : in    std_logic;
-            sys_bus_i       : in    sbus_in;
-            sys_bus_o       : out   sbus_out;
-            emag_enb        : out   io_o;
-            emag_out_1      : out   io_o;
-            emag_out_2      : out   io_o
+            clk                 : in    std_logic;
+            rst                 : in    std_logic;
+            sys_bus_i           : in    sbus_in;
+            sys_bus_o           : out   sbus_out;
+            p_em_enb            : out   io_o;
+            p_em_out_1          : out   io_o;
+            p_em_out_2          : out   io_o
         );
     end component;
 
 
     --****INTERNAL SIGNALS****
     --Simulation timing
-    constant clk_period		:	time := 10 ns;
+    constant clk_period		:	time := 20 ns;
 	signal reset			:	std_logic := '0';
 	signal clock			:	std_logic := '0';
 	signal run_sim			:	std_logic := '1';
     --DUT IOs
-    signal sys_bus_i        :   sbus_in;
-    signal sys_bus_o        :   sbus_out;
-    signal emag_enb         :   io_o;
-    signal emag_out_1       :   io_o;
-    signal emag_out_2       :   io_o;
+    signal sys_bus_i        :   sbus_in  := gnd_sbus_i;
+    signal sys_bus_o        :   sbus_out := gnd_sbus_o;
+    signal p_em_enb         :   io_o := low_io_o;
+    signal p_em_out_1       :   io_o := low_io_o;
+    signal p_em_out_2       :   io_o := low_io_o;
 
   
 begin
 
     --****COMPONENT****
     -----------------------------------------------------------------------------------------------
-    DUT : EMAGNET_DRIVER_2
+    DUT : EMAGNET_SMODULE_2
     generic map(
-        ADDRESS         => 1,
-        MAGNET_TAO      => 1000,
-        PULSE_REDUCTION => 1000
+        g_address           => 1,
+        g_magnet_tao        => 1000,
+        g_pulse_width       => 5000,
+        g_pulse_reduction   => 1000
     )
     port map(
-        clk             => clock,
-        rst             => reset,
-        sys_bus_i       => sys_bus_i,
-        sys_bus_o       => sys_bus_o,
-        emag_enb        => emag_enb,
-        emag_out_1      => emag_out_1,
-        emag_out_2      => emag_out_2
+        clk                 => clock,
+        rst                 => reset,
+        sys_bus_i           => sys_bus_i,
+        sys_bus_o           => sys_bus_o,
+        p_em_enb            => p_em_enb,
+        p_em_out_1          => p_em_out_1,
+        p_em_out_2          => p_em_out_2
     );
     -----------------------------------------------------------------------------------------------
 
@@ -104,7 +110,7 @@ begin
     --****SIMULATION TIMING****
     -----------------------------------------------------------------------------------------------
 	clock <= run_sim and (not clock) after clk_period/2;
-	reset <= '1' after 5 ns, '0' after 15 ns;
+	reset <= '1' after 10 ns, '0' after 30 ns;
     -----------------------------------------------------------------------------------------------
 
 
@@ -114,36 +120,35 @@ begin
     TEST : process
         variable init_hold      :   time := 5*clk_period/2;
         variable assert_hold    :   time := 3*clk_period/2;
-        variable post_hold      :   time := clk_period/2;
+        variable post_hold      :   time := 1*clk_period/2;
     begin
-        --Preset signals
-        sys_bus_i <= gnd_sbus_i;
+        --**Initial setup**
         wait for init_hold;
 
 
         --**Test reset state**
         wait for assert_hold;
-        assert(emag_enb = ('1','0'))
-            report "ID01: Test reset - expecting emag_enb = (1,0)" severity error;
-        assert(emag_out_1 = ('1','0'))
-            report "ID02: Test reset - expecting emag_out_1 = (1,0)" severity error;
-        assert(emag_out_2 = ('1','0'))
-            report "ID03: Test reset - expecting emag_out_2 = (1,0)" severity error;
+        assert(p_em_enb = ('1','0'))
+            report "ID01: Test reset - expecting p_em_enb = (1,0)" severity error;
+        assert(p_em_out_1 = ('1','0'))
+            report "ID02: Test reset - expecting p_em_out_1 = (1,0)" severity error;
+        assert(p_em_out_2 = ('1','0'))
+            report "ID03: Test reset - expecting p_em_out_2 = (1,0)" severity error;
         wait for post_hold;
 
 
         --**Test power on**
-        sys_bus_i <= writeBus(1,128);
+        sys_bus_i <= writeBus(1,1);
         wait for clk_period;
         sys_bus_i <= gnd_sbus_i;
 
         wait for assert_hold;
-        assert(emag_enb = ('1','1'))
-            report "ID04: Test power on - expecting emag_enb = (1,1)" severity error;
-        assert(emag_out_1 = ('1','1'))
-            report "ID05: Test power on - expecting emag_out_1 = (1,1)" severity error;
-        assert(emag_out_2 = ('1','0'))
-            report "ID06: Test power on - expecting emag_out_2 = (1,0)" severity error;
+        assert(p_em_enb = ('1','1'))
+            report "ID04: Test power on - expecting p_em_enb = (1,1)" severity error;
+        assert(p_em_out_1 = ('1','1'))
+            report "ID05: Test power on - expecting p_em_out_1 = (1,1)" severity error;
+        assert(p_em_out_2 = ('1','0'))
+            report "ID06: Test power on - expecting p_em_out_2 = (1,0)" severity error;
         wait for post_hold;
 
 
@@ -152,7 +157,7 @@ begin
 
 
         --**Test power off**
-        sys_bus_i <= writeBus(1,5);
+        sys_bus_i <= writeBus(1,0);
         wait for clk_period;
         sys_bus_i <= gnd_sbus_i;
 
@@ -160,64 +165,64 @@ begin
         for i in 1 to 5 loop
 
             for j in 1 to 10 loop
-                assert(emag_enb = ('1','0'))
-                    report "ID07: Test power off - expecting emag_enb = (1,0) ["
+                assert(p_em_enb = ('1','0'))
+                    report "ID07: Test power off - expecting p_em_enb = (1,0) ["
                     & integer'image(i) & "," & integer'image(j) & "]"
                     severity error;
-                assert(emag_out_1 = ('1','0'))
-                    report "ID08: Test power off - expecting emag_out_1 = (1,0) ["
+                assert(p_em_out_1 = ('1','0'))
+                    report "ID08: Test power off - expecting p_em_out_1 = (1,0) ["
                     & integer'image(i) & "," & integer'image(j) & "]"
                     severity error;
-                assert(emag_out_2 = ('1','0'))
-                    report "ID09: Test power off - expecting emag_out_2 = (1,0)["
+                assert(p_em_out_2 = ('1','0'))
+                    report "ID09: Test power off - expecting p_em_out_2 = (1,0)["
                     & integer'image(i) & "," & integer'image(j) & "]"
                     severity error;
                 wait for 100*clk_period;
             end loop;
 
             for j in 1 to (6-i) loop
-                assert(emag_enb = ('1','1'))
-                    report "ID10: Test power off - expecting emag_enb = (1,1)["
+                assert(p_em_enb = ('1','1'))
+                    report "ID10: Test power off - expecting p_em_enb = (1,1)["
                     & integer'image(i) & "," & integer'image(j) & "]"
                     severity error;
-                assert(emag_out_1 = ('1','0'))
-                    report "ID11: Test power off - expecting emag_out_1 = (1,0)["
+                assert(p_em_out_1 = ('1','0'))
+                    report "ID11: Test power off - expecting p_em_out_1 = (1,0)["
                     & integer'image(i) & "," & integer'image(j) & "]"
                     severity error;
-                assert(emag_out_2 = ('1','1'))
-                    report "ID12: Test power off - expecting emag_out_2 = (1,1)["
+                assert(p_em_out_2 = ('1','1'))
+                    report "ID12: Test power off - expecting p_em_out_2 = (1,1)["
                     & integer'image(i) & "," & integer'image(j) & "]"
                     severity error;
                 wait for 1000*clk_period;
             end loop;
 
             for j in 1 to 10 loop
-                assert(emag_enb = ('1','0'))
-                    report "ID14: Test power off - expecting emag_enb = (1,0) ["
+                assert(p_em_enb = ('1','0'))
+                    report "ID14: Test power off - expecting p_em_enb = (1,0) ["
                     & integer'image(i) & "," & integer'image(j) & "]"
                     severity error;
-                assert(emag_out_1 = ('1','0'))
-                    report "ID15: Test power off - expecting emag_out_1 = (1,0) ["
+                assert(p_em_out_1 = ('1','0'))
+                    report "ID15: Test power off - expecting p_em_out_1 = (1,0) ["
                     & integer'image(i) & "," & integer'image(j) & "]"
                     severity error;
-                assert(emag_out_2 = ('1','0'))
-                    report "ID16: Test power off - expecting emag_out_2 = (1,0)["
+                assert(p_em_out_2 = ('1','0'))
+                    report "ID16: Test power off - expecting p_em_out_2 = (1,0)["
                     & integer'image(i) & "," & integer'image(j) & "]"
                     severity error;
                 wait for 100*clk_period;
             end loop;
         
             for j in 1 to (5-i) loop
-                assert(emag_enb = ('1','1'))
-                    report "ID17: Test power off - expecting emag_enb = (1,1)["
+                assert(p_em_enb = ('1','1'))
+                    report "ID17: Test power off - expecting p_em_enb = (1,1)["
                     & integer'image(i) & "," & integer'image(j) & "]"
                     severity error;
-                assert(emag_out_1 = ('1','1'))
-                    report "ID18: Test power off - expecting emag_out_1 = (1,0)["
+                assert(p_em_out_1 = ('1','1'))
+                    report "ID18: Test power off - expecting p_em_out_1 = (1,0)["
                     & integer'image(i) & "," & integer'image(j) & "]"
                     severity error;
-                assert(emag_out_2 = ('1','0'))
-                    report "ID19: Test power off - expecting emag_out_2 = (1,1)["
+                assert(p_em_out_2 = ('1','0'))
+                    report "ID19: Test power off - expecting p_em_out_2 = (1,1)["
                     & integer'image(i) & "," & integer'image(j) & "]"
                     severity error;
                 wait for 1000*clk_period;
@@ -225,8 +230,12 @@ begin
         end loop;
 
 
-        --End simulation
-        wait for 50 ns;
+		--**End simulation**
+		wait for 50 ns;
+        report "EMAGNET_SMODULE_2_TB - testbench completed";
+        --Simulation end usign vhdl2008 env library (Pipeline use)
+       	--std.env.finish;
+        --Simulation end for local use in lattice diamond software (VHDL2008 libraries supported)
         run_sim <= '0';
         wait;
 
