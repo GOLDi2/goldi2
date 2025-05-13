5d75d1c62f8b2189cbfd959e9b5ac6f9054a9d7b
Johannes Nau <johannes.nau@tu-ilmenau.de>
2025-05-13 06:58:15 +0000

    update stepper motor test tool

--- hardware/common/spi-test-tools/stepper_motor_v2.py
+++ hardware/common/spi-test-tools/stepper_motor_v2.py
@@ -1,54 +1,62 @@
 #!/usr/bin/env python3
 import asyncio
 import struct
+import sys
 import termios
 import tty
-#from fake_registers import SpiRegisters
-from spi_driver import SpiRegisters
-import sys
 from math import sqrt
 
 from pyee.asyncio import AsyncIOEventEmitter
+#from fake_registers import SpiRegisters
+from spi_driver import SpiRegisters
 
 
 class StepperMotor(AsyncIOEventEmitter):
     def __init__(
-        self, registers: SpiRegisters, address: int, max_speed: float=0.5
+        self, registers: SpiRegisters, address: int, max_speed: int=0xffff, max_acceleration: int=0xffff,
     ):
         super().__init__()
         self._registers = registers
         self._address = address
         self._max_speed = max_speed
+        self._max_acceleration= max_acceleration
         self._target_position = 0
         self._control_mode = "speed"
         self._control_speed = 0
         self.max_speed = 0
         self.slow_down_distance = 0
+        self.positions = []
 
         self._registers.add_register(self._address, self._calculatePostitionRegisters)
-        for r in range(self._address+6, self._address+16):
+        for r in range(self._address+6, self._address+13):
             self._registers.add_register(r, self._calculatePostitionRegisters)
 
-    def _set_speed_register(self, speed: float):
-        speed = int(abs(speed) * self._max_speed * 0xFFFF)
-        self._registers[self._address + 1] = speed & 0xFF
-        self._registers[self._address + 2] = (speed >> 8) & 0xFF
-
-    def _set_position_stop_register(self, position: int):
-        self._registers[self._address + 10] = position & 0xFF
-        self._registers[self._address + 11] = (position >> 8) & 0xFF
-
-    def _set_position_slow_down_register(self, position: int):
-        self._registers[self._address + 12] = position & 0xFF
-        self._registers[self._address + 13] = (position >> 8) & 0xFF
-
     def _get_encoder_division(self):
-        raw_division = self._registers[self._address+6] + (self._registers[self._address + 7] << 8)
+        raw_division = self._registers[self._address+1] + (self._registers[self._address + 2] << 8)
         return struct.unpack('e', struct.pack('H',raw_division))[0] # convert short to float
     
-    def _get_acceleration(self):
-        raw_division = self._registers[self._address+8] + (self._registers[self._address + 9] << 8)
-        return struct.unpack('e', struct.pack('H',raw_division))[0]*0xFFFF # convert short to float and convert from kHz to Hz
+    def getPosition(self):
+        return self._registers[self._address+3] + (self._registers[self._address + 4] << 8)
+
+    def _set_speed_register(self, speed: float):
+        speed = int(abs(speed) * self._max_speed)
+        self._registers[self._address + 5] = speed & 0xFF
+        self._registers[self._address + 6] = (speed >> 8) & 0xFF
+
+    def set_acceleration(self, acceleration: float):
+        acceleration = int(abs(acceleration) * self._max_acceleration)
+        self._registers[self._address + 7] = acceleration & 0xFF
+        self._registers[self._address + 8] = (acceleration >> 8) & 0xFF
+
+    def _set_position_stop_register(self, position: int):
+        self._registers[self._address + 9] = position & 0xFF
+        self._registers[self._address + 10] = (position >> 8) & 0xFF
+
+    def _set_position_slow_down_register(self, position: int):
+        self._registers[self._address + 11] = position & 0xFF
+        self._registers[self._address + 12] = (position >> 8) & 0xFF
+
+
 
     def setSpeed(self, value: float):
         self._control_mode = "speed"
@@ -75,7 +83,7 @@ class StepperMotor(AsyncIOEventEmitter):
             # stop ----------------------------,|    ||
             #                                  ||    ||
             self._registers[self._address] = 0b10000000
-        
+
         self._set_speed_register(value)
 
     def getSpeed(self):
@@ -89,8 +97,6 @@ class StepperMotor(AsyncIOEventEmitter):
 
         self._calculatePostitionRegisters(self._registers, True)
 
-    def getPosition(self):
-        return self._registers[self._address+14] + (self._registers[self._address + 15] << 8)
     
     def getMode(self):
         return self._control_mode
@@ -105,6 +111,8 @@ class StepperMotor(AsyncIOEventEmitter):
         position = self.getPosition()
         stop_position = self._target_position
         distance = stop_position - position
+
+        self.positions.append(distance)
 
         if distance > 0:
             # dir0 -----------------------------------,
@@ -130,13 +138,19 @@ class StepperMotor(AsyncIOEventEmitter):
         self._set_speed_register(self._control_speed)
 
         encoder_division = self._get_encoder_division()
+        #acceleration = self._get_acceleration()
+        #acceleration=2**16*128/2048*2**12
+        #acceleration=6.21359407407407*10**6
         acceleration = 1024/2048
 
         try:
             max_speed_squared = min((self._control_speed * self._max_speed * 0xFFFF)**2, acceleration * abs(distance) / encoder_division )
+            #max_speed_squared = (self._control_speed * self._max_speed * 0xFFFF)**2
+            max_speed = sqrt(max_speed_squared)
+            #slow_down_distance = (max_speed_squared + max_speed) / (2 * acceleration) * encoder_division
             slow_down_distance = max_speed_squared / (2 * acceleration) * encoder_division
             self.slow_down_distance = slow_down_distance
-            self.max_speed = sqrt(max_speed_squared)
+            self.max_speed = max_speed
         except ZeroDivisionError:
             slow_down_distance = 0
         if distance < 0:
@@ -146,21 +160,23 @@ class StepperMotor(AsyncIOEventEmitter):
 
         self._set_position_stop_register(int(stop_position))
         self._set_position_slow_down_register(int(slow_down_postion))
-        
-        
-    
+
+
+
 
 
 registers: SpiRegisters
 xMotor: StepperMotor
 start_address: int
-speed: int = 0
+speed: float = 0
+acceleration: float = 0
 dir = "stop"
 mode = "speed"
 target_pos = 0
 
 async def input_coroutine():
     global speed
+    global acceleration
     global dir
     global mode
     global target_pos
@@ -182,6 +198,22 @@ async def input_coroutine():
             speed -= 0.01
         if char == b'Q':
             speed -= 0.1
+
+        if char == b'x':
+            acceleration += 0.001
+        if char == b'X':
+            acceleration += 0.1
+        if char == b'y':
+            acceleration -= 0.001
+        if char == b'Y':
+            acceleration -= 0.1
+
+        if acceleration < 0:
+            acceleration = 0
+        if acceleration > 1:
+            acceleration = 1
+
+        xMotor.set_acceleration(acceleration)
 
         if char == b's':
             if mode == "position":
@@ -224,7 +256,7 @@ async def input_coroutine():
         if target_pos > 65535:
             target_pos = 65535
 
-        
+
         if mode == "speed":
             if dir == "stop":
                 xMotor.setSpeed(0)
@@ -241,31 +273,35 @@ async def output_coroutine():
         print("Mode:", mode,"      ")
         print("Position:", xMotor.getPosition(),"      ")
         print("Speed:", int(speed*100),"%      ")
+        print("Acceleration:", int(acceleration*100),"%      ")
         print("Max Speed:", int(xMotor.max_speed),"       ")
         print("Slow Down Distance:", int(xMotor.slow_down_distance),"       ")
+        print("Distances:", xMotor.positions[-10:], "\x1b[K")
         if mode == "speed":
             print("Direction:", dir,"      ")
             print()
             print("Press e/E to increase speed; Press q/Q to decrease speed")
+            print("Press y/Y to increase acceleration; Press x/X to decrease acceleration")
             print("Press m to switch to position control")
             print("Press s to stop; Press a to drive left; Press d to drive right", "                                                  ")
         else:
             print("Target Position:", target_pos,"      ")
             print()
             print("Press e/E to increase speed; Press q/Q to decrease speed")
+            print("Press y/Y to increase acceleration; Press x/X to decrease acceleration")
             print("Press m to switch to speed control   ")
             print("Press s to drive to target position; Press a/A to decrease target position; Press d/D to increase target position")
         print()
 
         des=["CTRL",
-             "SPEED[0]", "SPEED[1]",
-             "SPI[0]", "SPI[1]", "SPI[2]",
              "ENC_DIVISION[0]", "ENC_DIVISION[1]",
+             "POSITION[0]", "POSITION[1]",
+             "SPEED[0]", "SPEED[1]",
              "ACCELERATION[0]", "ACCELERATION[1]",
              "POSITION_STOP[0]", "POSITION_STOP[1]",
              "POSITION_SD[0]", "POSITION_SD[1]",
-             "POSITION[0]", "POSITION[1]"]
-        for r in range(start_address, start_address+16):
+             ]
+        for r in range(start_address, start_address+13):
             print("Register " + str(r).rjust(3) + " = 0b", end="")
             print("{0:b} ".format(registers[r]).zfill(9), end="")
             print(des[r-start_address])
@@ -282,7 +318,7 @@ async def main_async():
     except:
         print("Usage: stepper_motor_v2.py <start_address>")
         return
-    
+
     registers = SpiRegisters()
 
     #registers[start_address+7]=0x41
