508a4db04dca0cdefd4336f69c0e05932d6ec57a
Pierre Helbing <pierre.helbing@tu-ilmenau.de>
2025-06-26 13:50:36 +0000

    always build extensions before copying

--- frontend-services/cwide/scripts/copy-extensions.sh
+++ frontend-services/cwide/scripts/copy-extensions.sh
@@ -7,42 +7,49 @@ rm -rf public/extensions
 
 mkdir public/extensions
 
+npm --prefix extensions/base run build
 mkdir public/extensions/base
 mkdir public/extensions/base/dist
 cp -r extensions/base/src public/extensions/base/src
 cp -r extensions/base/dist-web public/extensions/base/dist/web
 cp extensions/base/package.json public/extensions/base
 
+npm --prefix extensions/collaboration run build
 mkdir public/extensions/collaboration
 mkdir public/extensions/collaboration/dist
 cp -r extensions/collaboration/src public/extensions/collaboration/src
 cp -r extensions/collaboration/dist-web public/extensions/collaboration/dist/web
 cp extensions/collaboration/package.json public/extensions/collaboration
 
+npm --prefix extensions/compilation run build
 mkdir public/extensions/compilation
 mkdir public/extensions/compilation/dist
 cp -r extensions/compilation/src public/extensions/compilation/src
 cp -r extensions/compilation/dist-web public/extensions/compilation/dist/web
 cp extensions/compilation/package.json public/extensions/compilation
 
+npm --prefix extensions/debugging run build
 mkdir public/extensions/debugging
 mkdir public/extensions/debugging/dist
 cp -r extensions/debugging/src public/extensions/debugging/src
 cp -r extensions/debugging/dist-web public/extensions/debugging/dist/web
 cp extensions/debugging/package.json public/extensions/debugging
 
+npm --prefix extensions/filesystem run build
 mkdir public/extensions/filesystem
 mkdir public/extensions/filesystem/dist
 cp -r extensions/filesystem/src public/extensions/filesystem/src
 cp -r extensions/filesystem/dist-web public/extensions/filesystem/dist/web
 cp extensions/filesystem/package.json public/extensions/filesystem
 
+npm --prefix extensions/lsp run build
 mkdir public/extensions/lsp
 mkdir public/extensions/lsp/dist
 cp -r extensions/lsp/src public/extensions/lsp/src
 cp -r extensions/lsp/dist-web public/extensions/lsp/dist/web
 cp extensions/lsp/package.json public/extensions/lsp
 
+npm --prefix extensions/testing run build
 mkdir public/extensions/testing
 mkdir public/extensions/testing/dist
 cp -r extensions/testing/src public/extensions/testing/src
